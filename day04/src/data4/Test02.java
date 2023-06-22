d76a185a28e76726f1e534a7b3c2ba41bbfd7da8
jhcho <jhcho@LenovoIDEAPAD>
2023-06-22 10:40:08 +0900

    day04

---
+++ day04/src/data4/Test02.java
@@ -0,0 +1,14 @@
+package data4;
+
+public class Test02 {
+
+	public static void main(String[] args) {
+		// 문자열(String)
+		// 		문자가 모여있는 형태의 데이터
+
+	
+		String a = "Hello";
+		System.out.println(a);
+	}
+
+}
--- kh/src/kh/Test.java
+++ kh/src/kh/Test.java
@@ -3,7 +3,7 @@ package kh;
 public class Test {
 
 	public static void main(String[] args) {
-		System.out.println("Hello, World!");
+		// 윤년
+		
 	}
-
 }
