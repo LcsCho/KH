d7ec508a768206efc257bb7db329bef1299a1e92
jhcho <jhcho@LenovoIDEAPAD>
2023-06-22 13:42:12 +0900

    practice

---
+++ day04/src/practice/Practice01.java
@@ -0,0 +1,24 @@
+package practice;
+
+import java.util.Scanner;
+
+public class Practice01 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("국어, 영어, 수학의 점수를 입력하시오.");
+		int kor = sc.nextInt();
+		int eng = sc.nextInt();
+		int mat = sc.nextInt();
+		int tot = kor + eng + mat;
+		double avg = tot / 3;
+		
+		System.out.println("총점: " + tot);
+		System.out.println("평균: " + avg);
+		
+		
+		sc.close();
+	}
+
+}
