35d55cc40e16234c370f3adc92a8819a9001dfd6
jhcho <jhcho@LenovoIDEAPAD>
2023-06-24 00:50:48 +0900

    day05

---
+++ day05/src/loop/Test02.java
@@ -0,0 +1,22 @@
+package loop;
+
+import java.util.Scanner;
+
+public class Test02 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("숫자 입력 : ");
+		int a = sc.nextInt();
+		System.out.print("숫자 입력 : ");
+		int b = sc.nextInt();
+		System.out.print("숫자 입력 : ");
+		int c = sc.nextInt();
+		System.out.print("숫자 입력 : ");
+		int d = sc.nextInt();
+		System.out.print("숫자 입력 : ");
+		int e = sc.nextInt();
+		
+		sc.close();
+	}
+}
---
+++ day05/src/loop/Test03.java
@@ -0,0 +1,14 @@
+package loop;
+
+public class Test03 {
+	public static void main(String[] args) {
+		//1부터 100사이 홀수 출력
+		//- 1, 3, 5, 7, 9, ..., 93, 95, 97, 99 (50개)
+		//- 횟수로 처리하기 어려우므로 범위를 정해서 처리
+		
+		//for(1부터 99까지 2씩 늘어나도록) {
+		for(int i=1; i <=99; i+=2) {
+			System.out.println(i);
+		}
+	}
+}
