be70e7e0c42c2179053db3cb94808d84ea45bf5b
jhcho <jhcho@LenovoIDEAPAD>
2023-06-26 10:57:01 +0900

    day06

--- day06/src/loop/Test07.java
+++ day06/src/loop/Test07.java
@@ -1,5 +1,27 @@
 package loop;
 
-public class Test07 {
+import java.util.Scanner;
 
+public class Test07 {
+	public static void main(String[] args) throws InterruptedException {
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("분을 입력: ");
+		int min = sc.nextInt();
+		System.out.print("초를 입력: ");
+		int sec = sc.nextInt();
+		int totSec = 60 * min + sec;
+		
+		for (int i = totSec; i > 0; i--) {
+			System.out.println(totSec / 60 + "분" + totSec % 60 + "초 후 알람이 울립니다!");
+			totSec--;
+			
+			
+			// 1초 대기
+			if (i > 0)
+				Thread.sleep(1000);
+			
+			
+		}
+	}
 }
