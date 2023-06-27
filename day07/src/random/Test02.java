5fa6312ad4d056f05f74ced62113a7ac615ae153
jhcho <jhcho@LenovoIDEAPAD>
2023-06-27 10:32:59 +0900

    day07

--- day07/src/random/Test02.java
+++ day07/src/random/Test02.java
@@ -9,19 +9,19 @@ public class Test02 {
 		int lotto = r.nextInt(45) + 1;
 		System.out.println(lotto);
 		
-		int number = r.nextInt(99) + 10;
+		int number = r.nextInt(90) + 10;
 		System.out.println(number);
 		
-		int otp = r.nextInt(9);
+		int otp = r.nextInt(1000000);
 		System.out.println(otp);
 	
-		int fOrB = r.nextInt(2) + 1;
-		if (fOrB == 1) System.out.println("앞");
+		int fOrB = r.nextInt(2);
+		if (fOrB == 0) System.out.println("앞");
 		else System.out.println("뒤");
 		
-		int rcp = r.nextInt(3) + 1;
-		if (rcp == 1) System.out.println("가위!");
-		else if (rcp == 2) System.out.println("바위!");
+		int rcp = r.nextInt(3);
+		if (rcp == 0) System.out.println("가위!");
+		else if (rcp == 1) System.out.println("바위!");
 		else System.out.println("보!");
 	}
 }
