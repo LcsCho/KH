e4ee57544a403812f905fb3845d58985b19751ec
jhcho <jhcho@LenovoIDEAPAD>
2023-06-28 09:53:21 +0900

    day08

---
+++ day08/src/array/Test02.java
@@ -0,0 +1,19 @@
+package array;
+
+public class Test02 {
+	public static void main(String[] args) {
+		// 배열(Array) 
+		// - 여러 개의 같은 종류 데이터를 저장할 수 있는 공간
+		
+		// 생성
+		int[] a = new int[3];
+		
+		System.out.println(a);
+		System.out.println(a[0]);
+		System.out.println(a[1]);
+		System.out.println(a[2]);
+		
+		
+		
+	}
+}
