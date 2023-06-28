cb1e3d307ef7cc80128b70effb368a9fb16c5351
jhcho <jhcho@LenovoIDEAPAD>
2023-06-28 15:54:52 +0900

    day08

--- day08/src/array/Test10.java
+++ day08/src/array/Test10.java
@@ -2,6 +2,12 @@ package array;
 
 public class Test10 {
 	public static void main(String[] args) {
+		// 배열의 주요 계산 방법 - 최대/최소 찾기
+		// - data의 가장 큰 값이 얼마인가? 어디있는가?
+		// - data의 가장 작은 값이 얼마인가? 어디있는가?
+		
+		int[] data = new int[] {30, 50, 20, 10, 40};
+		
 		
 	}
 }
