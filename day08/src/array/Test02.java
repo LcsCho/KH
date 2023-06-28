85b67679255250353a844217b4fb37502e6daf35
jhcho <jhcho@LenovoIDEAPAD>
2023-06-28 09:59:46 +0900

    day08

--- day08/src/array/Test02.java
+++ day08/src/array/Test02.java
@@ -8,12 +8,16 @@ public class Test02 {
 		// 생성
 		int[] a = new int[3];
 		
-		System.out.println(a);
-		System.out.println(a[0]);
-		System.out.println(a[1]);
-		System.out.println(a[2]);
+		// a = 10; // error
+		a[0] = 10;
+		a[1] = 20;
+		a[2] = 30;
+		// a[3] = 40; // 실행하면 error(런타임 에러)
 		
-		
-		
+		// System.out.println(a); // 리모컨 정보 출력
+		System.out.println(a[0]); // a가 보는 +0지점 데이터
+		System.out.println(a[1]); // a가 보는 +1지점 데이터
+		System.out.println(a[2]); // a가 보는 +2지점 데이터
+		// System.out.println(a[3]); // 없음(error);
 	}
 }
