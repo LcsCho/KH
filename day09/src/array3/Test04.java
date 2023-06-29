41fe40a4375676f5bf12478fda12b1e325cf2784
jhcho <jhcho@LenovoIDEAPAD>
2023-06-29 15:03:43 +0900

    day09

---
+++ day09/src/array3/Test04.java
@@ -0,0 +1,13 @@
+package array3;
+
+public class Test04 {
+	public static void main(String[] args) {
+		String[][] arr = new String[][] {{"자바", "스프링", "안드로이드"}, {"파이썬", "판다스", "장고"}};
+		
+		for (int i = 0; i < arr.length; i++) {
+			for (int j = 0; j < arr[0].length; j++) {
+				System.out.println(arr[i][j]);
+			}
+		}
+	}
+}
