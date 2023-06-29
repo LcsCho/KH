e860713c0c85897e820e4faba6389278353cf7cf
jhcho <jhcho@LenovoIDEAPAD>
2023-06-29 12:36:01 +0900

    day09

--- day09/src/array2/Test01.java
+++ day09/src/array2/Test01.java
@@ -1,5 +1,46 @@
 package array2;
 
 public class Test01 {
-
+	public static void main(String[] args) {
+		// 정렬(sort)
+		// - 여러 개의 데이터를 원하는 목적에 맞게 나열하는 것
+		// - 변호순, 이름순, 가격순, 랭킹순, 성적순, ...
+		// - 번호순이라고 하면 번호가 낮은 것부터 나와야 한다.(오름차순)
+		// - 성적순이라고 하면 성적이 높은 것부터 나와야한다.(내림차순)
+		// - 정렬 방법은 여러가지가 있으며 상황에 따라 성능이 다르다.
+		// - 사용자에게 데이터를 효과적으로 보여주기 위한 수단
+		
+		// 데이터 준비
+		int[] data = new int[] {30, 50, 20, 10, 40};
+		
+		// 선택정렬
+		for (int i = 0; i < data.length - 1; i++) {
+			for (int j = i + 1; j < data.length; j++) {
+				if (data[i] > data[j]) {
+					int temp = data[i];
+					data[i] = data[j];
+					data[j]= temp;  
+				}
+			}
+		}
+		
+		for (int i = 0; i < data.length; i++) 
+			System.out.print(data[i] + " ");
+		
+		System.out.println();
+		data = new int[] {30, 50, 20, 10, 40};
+		
+		// 버블정렬
+		for (int i = 0; i < data.length - 1; i++) {
+			for (int j = 1; j < data.length; j++) {
+				if (data[j] < data[j - 1]) {
+					int temp = data[j - 1];
+					data[j - 1] = data[j];
+					data[j]= temp; 
+				}			
+			}
+		}
+		for (int i = 0; i < data.length; i++) 
+			System.out.print(data[i] + " ");
+	}
 }
