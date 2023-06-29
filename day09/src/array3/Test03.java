046aa660a1b35493a223d0c81e664328fd725e8d
jhcho <jhcho@LenovoIDEAPAD>
2023-06-29 15:45:23 +0900

    day09

--- day09/src/array3/Test03.java
+++ day09/src/array3/Test03.java
@@ -14,13 +14,13 @@ public class Test03 {
 		}
 		
 		for (int i = 0; i < arr.length; i++) {
-			for (int j = 0; j < arr[0].length; j++) {
+			for (int j = 0; j < arr[i].length; j++) {
 				arr[i][j] = sc.nextFloat();
 			}
 		}
 		
 		for (int i = 0; i < arr.length; i++) {
-			for (int j = 0; j < arr[0].length; j++) {
+			for (int j = 0; j < arr[i].length; j++) {
 				System.out.print(arr[i][j] + " ");
 			}
 			System.out.println();
