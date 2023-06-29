d5ccb67e465e872784addee90f6f9d1e8f126fb1
jhcho <jhcho@LenovoIDEAPAD>
2023-06-29 15:06:51 +0900

    day09

--- day09/src/array3/Test03.java
+++ day09/src/array3/Test03.java
@@ -2,12 +2,13 @@ package array3;
 
 public class Test03 {
 	public static void main(String[] args) {
-		double[][] arr = new double[][] {{1.5f, 2.5f, 3.6f}, {10.2f, 5.3f, 6.5f}};
+		float[][] arr = new float[][] {{1.5f, 2.5f, 3.6f}, {10.2f, 5.3f, 6.5f}};
 		
 		for (int i = 0; i < arr.length; i++) {
 			for (int j = 0; j < arr[0].length; j++) {
-				System.out.println(arr[i][j]);
+				System.out.print(arr[i][j] + " ");
 			}
+			System.out.println();
 		}
 	}
 }
--- day09/src/array3/Test04.java
+++ day09/src/array3/Test04.java
@@ -6,8 +6,9 @@ public class Test04 {
 		
 		for (int i = 0; i < arr.length; i++) {
 			for (int j = 0; j < arr[0].length; j++) {
-				System.out.println(arr[i][j]);
+				System.out.print(arr[i][j] + " ");
 			}
+			System.out.println();
 		}
 	}
 }
