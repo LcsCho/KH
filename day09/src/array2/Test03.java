9b4dfbc7cae2915c7f8748f162c12b5921fce54a
jhcho <jhcho@LenovoIDEAPAD>
2023-06-29 14:59:05 +0900

    day09

--- day09/src/array2/Test03.java
+++ day09/src/array2/Test03.java
@@ -1,5 +1,13 @@
 package array2;
 
-public class Test03 {
+import java.util.Arrays;
 
+public class Test03 {
+	public static void main(String[] args) {
+		int[] data = new int[] {20, 10, 30, 40, 50};
+		
+		Arrays.sort(data);
+		
+		System.out.println(Arrays.toString(data));
+	}
 }
