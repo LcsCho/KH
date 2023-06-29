d168b70a51fe9aef2baef7feeca698f8d89945a8
jhcho <jhcho@LenovoIDEAPAD>
2023-06-29 15:45:18 +0900

    day09

--- day09/src/array3/Test03.java
+++ day09/src/array3/Test03.java
@@ -1,8 +1,23 @@
 package array3;
 
+import java.util.Scanner;
+
 public class Test03 {
 	public static void main(String[] args) {
 		float[][] arr = new float[][] {{1.5f, 2.5f, 3.6f}, {10.2f, 5.3f, 6.5f}};
+		Scanner sc = new Scanner(System.in);
+		for (int i = 0; i < arr.length; i++) {
+			for (int j = 0; j < arr[0].length; j++) {
+				System.out.print(arr[i][j] + " ");
+			}
+			System.out.println();
+		}
+		
+		for (int i = 0; i < arr.length; i++) {
+			for (int j = 0; j < arr[0].length; j++) {
+				arr[i][j] = sc.nextFloat();
+			}
+		}
 		
 		for (int i = 0; i < arr.length; i++) {
 			for (int j = 0; j < arr[0].length; j++) {
--- day09/src/array3/Test04.java
+++ day09/src/array3/Test04.java
@@ -5,7 +5,7 @@ public class Test04 {
 		String[][] arr = new String[][] {{"자바", "스프링", "안드로이드"}, {"파이썬", "판다스", "장고"}};
 		
 		for (int i = 0; i < arr.length; i++) {
-			for (int j = 0; j < arr[0].length; j++) {
+			for (int j = 0; j < arr[i].length; j++) {
 				System.out.print(arr[i][j] + " ");
 			}
 			System.out.println();
