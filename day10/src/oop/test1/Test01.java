b3d26993a6fbd6bd72415d5ac9b20aba4dd85894
jhcho <jhcho@LenovoIDEAPAD>
2023-06-30 10:19:12 +0900

    day10

--- day10/src/oop/test1/Test01.java
+++ day10/src/oop/test1/Test01.java
@@ -4,5 +4,18 @@ public class Test01 {
 	public static void main(String[] args) {
 		// 시험 성적 정보 1개를 저장
 		// 이름: 피카츄, 국어: 90, 영어: 80, 수학: 70
+		Grade g = new Grade();
+		
+		// g -> Grade 객체(이름, 국어, 영어, 수학)
+		
+		g.name = "피카츄";
+		g.korean = 90;
+		g.english = 80;
+		g.math = 70;
+		
+		System.out.println(g.name);
+		System.out.println(g.korean);
+		System.out.println(g.english);
+		System.out.println(g.math);
 	}
 }
