b57848ef1ac1502e8cd09d78e9c2d902c885aef6
jhcho <jhcho@LenovoIDEAPAD>
2023-06-30 14:12:07 +0900

    day10

--- day10/src/oop/method3/Mobile.java
+++ day10/src/oop/method3/Mobile.java
@@ -1,12 +1,12 @@
 package oop.method3;
 
 public class Mobile {
-	String type, name;
+	String name, telecom;
 	int price, data, call, message;
 	
-	void setup(String type, String name, int price, int data, int call, int message) {
-		this.type = type;
-		this.name = name;
+	void setup(String type, String telecom, int price, int data, int call, int message) {
+		this.name = type;
+		this.telecom = telecom;
 		this.price = price;
 		this.data = data;
 		this.call = call;
@@ -14,8 +14,8 @@ public class Mobile {
 	}
 	
 	void show() {
-		System.out.print("통신사: " + this.type + ", ");
-		System.out.print("상품명: " + this.name + ", ");
+		System.out.print("통신사: " + this.name + ", ");
+		System.out.print("상품명: " + this.telecom + ", ");
 		System.out.println("월정액: " + this.price);
 		System.out.print("데이터: " + this.data + ", ");
 		System.out.print("통화: " + this.call + ", ");
