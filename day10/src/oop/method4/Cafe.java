706645014073052a7755cb09f9de65f674817af9
jhcho <jhcho@LenovoIDEAPAD>
2023-06-30 14:57:10 +0900

    day10

--- day10/src/oop/method4/Cafe.java
+++ day10/src/oop/method4/Cafe.java
@@ -16,9 +16,9 @@ public class Cafe {
 		System.out.print("이름: " + this.name + ", ");
 		System.out.print("분류: " + this.type + ", ");
 		if (this.onSale == true) {
-			System.out.print("정상 가격: " + this.price + ", 할인 가격(20%할인) " + (int)(this.price * 0.8) + ", ");
+			System.out.print("정상 가격: " + this.price + "원, 할인 가격(20%할인) " + (int)(this.price * 0.8) + "원, ");
 			System.out.println("행사여부: 행사중");
 		}
-		else System.out.println("가격: " + this.price);
+		else System.out.println("가격: " + this.price + "원");
 	}
 }
