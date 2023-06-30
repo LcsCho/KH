3b8a782a1f3b9ff42bc6b96b740b36412c38edfe
jhcho <jhcho@LenovoIDEAPAD>
2023-06-30 12:55:55 +0900

    day10

--- day10/src/oop/method1/Player.java
+++ day10/src/oop/method1/Player.java
@@ -1,6 +1,20 @@
 package oop.method1;
 
+// 클래스
 public class Player {
+	// 멤버 필드(멤버 변수) = 데이터
 	int rank, goal;
 	String name, nation;
+	
+	// 멤버 메소드 = 공통코드
+	// void 이름(){코드}
+	void show() {
+		// this: 클래스의 멤버를 의미(나 자신)
+		System.out.println(this.rank + ", " + this.name + ", " + this.nation + ", " + this.goal);
+		
+	}
+	
+	
+	
+	
 }
--- day10/src/oop/method1/Test01.java
+++ day10/src/oop/method1/Test01.java
@@ -31,9 +31,9 @@ public class Test01 {
 		
 		// 출력
 		System.out.println("순위\t이름\t국가\t골\n--------------------------");
-		System.out.println(p1.rank + ", " + p1.name + ", " + p1.nation + ", " + p1.goal);
-		System.out.println(p2.rank + ", " + p2.name + ", " + p2.nation + ", " + p2.goal);
-		System.out.println(p3.rank + ", " + p3.name + ", " + p3.nation + ", " + p3.goal);
-		System.out.println(p3.rank + ", " + p4.name + ", " + p4.nation + ", " + p4.goal);
+		p1.show();
+		p2.show();
+		p3.show();
+		p4.show();
 	}
 }
