431af6dcc1ffab8309b9b24eb00395372711f5b8
jhcho <jhcho@LenovoIDEAPAD>
2023-06-30 13:07:02 +0900

    day10

--- day10/src/oop/method1/Player.java
+++ day10/src/oop/method1/Player.java
@@ -11,10 +11,15 @@ public class Player {
 	void show() {
 		// this: 클래스의 멤버를 의미(나 자신)
 		System.out.println(this.rank + ", " + this.name + ", " + this.nation + ", " + this.goal);
-		
 	}
 	
-	
-	
-	
+	// 초기화용
+	// - 설정될 값을 알 수 없으므로 외부에서 전달받아서 초기화
+	// - 매개변수
+	void setup(int rank, String name, String nation, int goal) {
+		this.rank = rank;
+		this.name = name;
+		this.nation = nation;
+		this.goal = goal;
+	}
 }
