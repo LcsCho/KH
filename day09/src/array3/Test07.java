eca80f62b97a1903e72e364dfe01c6a8352b82bb
jhcho <jhcho@LenovoIDEAPAD>
2023-06-30 09:42:42 +0900

    day09

--- day09/src/array3/Test07.java
+++ day09/src/array3/Test07.java
@@ -6,24 +6,68 @@ import java.util.Scanner;
 public class Test07 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		Random r = new Random();
-		System.out.print("지뢰찾기 배열의 크기(홀수): ");
-		
+		System.out.print("지뢰찾기 배열 입력(홀수): ");
 		int size = sc.nextInt();
-		sc.close();
+		int bomb = size + 1;
+		int[][] map = new int[size][size];
 		
-		int[][] mineList = new int[size][size];
-		for (int i = 0; i < size + 1; i++) {
-			int r1 = r.nextInt(size);
-			int r2 = r.nextInt(size);
-			
-			if (mineList[r1][r2] == 0) mineList[r1][r2] = 9;
-			else 
+		Random r = new Random();
+		
+		//폭탄 배치
+		for(int i=0; i < bomb; i++) {
+			int x = r.nextInt(size);
+			int y = r.nextInt(size);
+			if(map[x][y] == 0) {
+				map[x][y] = 9;
+			}
+			else {
 				i--;
+			}
 		}
-		for (int i = 0; i < mineList.length; i++) {
-			for (int j = 0; j < mineList[i].length; j++) {
-				System.out.print(mineList[i][j] + " ");
+		
+		//계산
+		for(int i=0; i < size; i++) {
+			for(int k=0; k < size; k++) {//전체 칸을 반복하며
+				if(map[i][k] == 9) continue;//폭탄이면 패스
+				
+				//탐색 범위 설정(왼쪽상단 - 오른쪽하단 위치)
+				int xmin = i - 1;
+				int xmax = i + 1;
+				int ymin = k - 1;
+				int ymax = k + 1;
+				
+				//모서리일 경우 추가 계산
+				if(xmin < 0) xmin = 0;
+				if(xmax > size - 1) xmax = size - 1;
+				if(ymin < 0) ymin = 0;
+				if(ymax > size - 1) ymax = size - 1;
+				
+				//확인용 멘트
+				System.out.println("("+i+", "+k+") 위치 검사중");
+				System.out.println("--> 범위 : ("+xmin+", "+ymin+") 부터 ("+xmax+", "+ymax+") 까지");
+				
+				//미리 지정한 범위에서 폭탄 찾기
+				int count = 0;
+				for(int p = xmin; p <= xmax; p++) {
+					for(int q = ymin; q <= ymax; q++) {
+						if(map[p][q] == 9) {//폭탄을 발견하면
+							count++;
+						}
+					}
+				}
+				map[i][k] = count;//개수를 설정
+			}
+		}
+		
+		//지도 출력
+		for(int i=0; i < size; i++) {
+			for(int k=0; k < size; k++) {
+				switch(map[i][k]) {
+				case 0: System.out.print(0); break;
+				case 9: System.out.print("◈"); break;
+				default: System.out.print(map[i][k]); break;
+				}
+				System.out.print(" ");
 			}
 			System.out.println();
 		}
