12092915faa9a38ac46ec0fde019836442a2cb65
jhcho <jhcho@LenovoIDEAPAD>
2023-07-03 16:48:27 +0900

    day11

--- day11/src/oop/constructor3/Test01.java
+++ day11/src/oop/constructor3/Test01.java
@@ -3,7 +3,7 @@ package oop.constructor3;
 public class Test01 {
 	public static void main(String[] args) {
 		Character c1 = new Character("헤라클레스", "전사", 50);
-		Character c2 = new Character("포세이돈", "마법사", 50);
+		Character c2 = new Character("포세이돈", "마법사", 20);
 		Character c3 = new Character("아프로디테", "마법사");
 		
 		System.out.println("<캐릭터 정보>");
