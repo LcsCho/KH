03b3e47c3e4dec3d3616097f842dc5442351360c
jhcho <jhcho@LenovoIDEAPAD>
2023-07-03 13:02:23 +0900

    day11

--- day11/src/oop/setter3/Phone.java
+++ day11/src/oop/setter3/Phone.java
@@ -20,17 +20,23 @@ public class Phone {
 			this.telecom = telecom;
 		}
 	}
-	private void price(int price) { if (price >= 0)	this.price = price; }
+	private void price(int price) { 
+		if (price < 0)	return;
+		this.price = price; }
 	private void month(int month) { if (month == 0 || month == 24 || month == 36) this.month = month; }
 	public void show() {
-		System.out.print("휴대폰명: " + this.name + ", 통신사: " + this.telecom);
+		System.out.print("제품명: " + this.name + ", 통신사: " + this.telecom);
 		if (month == 0) {
 			System.out.print(", 가격: " + this.price + "원(약정 없음)");
-			System.out.println(", 일시불(5% 할인): " + (int)(this.price * 0.95) + "원");
-		}
-		else {
+			System.out.println(", 일시불(5% 할인): " + this.getPrice() + "원");
+		} else {
 			System.out.print(", 가격: " + this.price + "원, 약정개월: " + this.month);
-			System.out.println(", 월 할부금: " + this.price / this.month + "원");
+			System.out.println(", 월 할부금: " + this.getPrice() + "원");
 		}
 	}
+	private int getPrice() {
+		if (month == 0) return (int)(this.price * 0.95);
+		else return this.price / this.month;
+	}
+
 }
