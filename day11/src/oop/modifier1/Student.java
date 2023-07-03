821e0797285e0ba835794ba7a77a4e75dead7ff2
jhcho <jhcho@LenovoIDEAPAD>
2023-07-03 17:08:26 +0900

    day11

--- day11/src/oop/modifier1/Student.java
+++ day11/src/oop/modifier1/Student.java
@@ -1,5 +1,23 @@
 package oop.modifier1;
 
 public class Student {
-
+	// 필드는 접근을 차단해야 한다 (private)
+	// - 같은 클래스 내부에서는 접근이 가능해야 한다.
+	private String name;
+	private int score;
+	
+	// 필드를 제외한 나머지는 공개(public)
+	public void setName(String name) {
+		this.name = name;
+	}
+	public void setScore(int score) {
+		if (score < 0 || score > 100) return;
+		this.score = score;
+	}
+	public String getName() {
+		return name;
+	}
+	public int getScore() {
+		return score;
+	}
 }
--- day11/src/oop/modifier1/Test01.java
+++ day11/src/oop/modifier1/Test01.java
@@ -2,6 +2,12 @@ package oop.modifier1;
 
 public class Test01 {
 	public static void main(String[] args) {
+		Student s = new Student();
 		
+		s.setName("피카츄");
+		s.setScore(50);
+		
+		System.out.println(s.getName());
+		System.out.println(s.getScore());
 	}
 }
