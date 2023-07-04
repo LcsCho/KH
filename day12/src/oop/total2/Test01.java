7fecdec04dbb6f7ad8bb0c6934ad8357158a9b05
jhcho <jhcho@LenovoIDEAPAD>
2023-07-04 18:20:43 +0900

    day12

--- day12/src/oop/total2/Test01.java
+++ day12/src/oop/total2/Test01.java
@@ -2,6 +2,13 @@ package oop.total2;
 
 public class Test01 {
 	public static void main(String[] args) {
+		Time t1 = new Time(3, 20, 20);
+		t1.show();
 		
+		Time t2 = new Time(40000);
+		t2.show();
+		
+		Time t3 = new Time(t1.getValue() + t2.getValue());
+		t3.show();
 	}
 }
--- day12/src/oop/total2/Time.java
+++ day12/src/oop/total2/Time.java
@@ -5,10 +5,7 @@ public class Time {
 	private long value;
 
 	// 필드에 대한 setter/getter
-	public long getValue() {
-		return value;
-	}
-
+	public long getValue() { return value; }
 	public void setValue(long value) {
 		if (value < 0L) return;
 		this.value = value;
@@ -24,4 +21,17 @@ public class Time {
 		this.setValue(total);
 		}
 	public Time(long second) { this.setValue(second); }
+	
+	// 가상의 getter 메서드
+	public long getHour() { return this.value / 60 / 60; }
+	public long getMinute() { return this.value / 60 % 60; }
+	public long getSecond() { return this.value % 60; }
+	public void show() {
+		System.out.print(this.getHour());
+		System.out.print("시간 ");
+		System.out.print(this.getMinute());
+		System.out.print("분 ");
+		System.out.print(this.getSecond());
+		System.out.println("초");
+	}
 }
