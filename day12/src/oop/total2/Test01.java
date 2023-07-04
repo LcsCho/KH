ae5048ee6168d7ae3294620b9fd49c141681a6f0
jhcho <jhcho@LenovoIDEAPAD>
2023-07-04 18:04:50 +0900

    day12

--- day12/src/oop/total2/Test01.java
+++ day12/src/oop/total2/Test01.java
@@ -1,5 +1,7 @@
 package oop.total2;
 
 public class Test01 {
-
+	public static void main(String[] args) {
+		
+	}
 }
--- day12/src/oop/total2/Time.java
+++ day12/src/oop/total2/Time.java
@@ -1,5 +1,27 @@
 package oop.total2;
 
 public class Time {
+	// 필드 - 초
+	private long value;
+
+	// 필드에 대한 setter/getter
+	public long getValue() {
+		return value;
+	}
+
+	public void setValue(long value) {
+		if (value < 0L) return;
+		this.value = value;
+	}
 	
+	// 생성자
+	public Time(long hour, long minute, long second) {
+		long total = hour * 60 * 60 + minute * 60 + second;
+		this.setValue(total);
+	}
+	public Time(long minute, long second) { 
+		long total = 60 * minute + second;
+		this.setValue(total);
+		}
+	public Time(long second) { this.setValue(second); }
 }
