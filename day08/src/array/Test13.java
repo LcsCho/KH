c791c1e959c9236eb6750540521af6aae9d32f03
jhcho <jhcho@LenovoIDEAPAD>
2023-07-04 13:53:09 +0900

    day08

--- day08/src/array/Test13.java
+++ day08/src/array/Test13.java
@@ -4,14 +4,16 @@ public class Test13 {
 	public static void main(String[] args) {
 		int[] arr = new int[] {30, 50, 20, 10, 40};
 
-
+		int left = 0;
+		int right = arr.length - 1;
 		
-		for (int i = 1; i <= arr.length / 2; i++) {
-			int left = i;
-			int right = arr.length - 1 - left;
-			int temp = arr[left];
+		for(int i=0; i < arr.length/2; i++) {
+			int backup = arr[left];
 			arr[left] = arr[right];
-			arr[right] = temp;
+			arr[right] = backup;
+			
+			left++;
+			right--;
 		}
 		
 		
