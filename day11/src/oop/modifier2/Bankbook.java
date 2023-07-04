0fc972a6c38cfc3f28b67ce1f0e04186fc3b88c1
jhcho <jhcho@LenovoIDEAPAD>
2023-07-04 09:30:45 +0900

    day11

---
+++ day11/src/oop/modifier2/Bankbook.java
@@ -0,0 +1,66 @@
+package oop.modifier2;
+
+import java.util.Scanner;
+
+public class Bankbook {
+	private String name;
+	private int period, count, pay, balance;
+	
+	public Bankbook(String name, int period, int count, int pay, int balance) {
+		this.setName(name);
+		this.setPeriod(period);
+		this.setCount(count);
+		this.setPay(pay);
+		this.setBalance(balance);
+	}
+	public Bankbook(String name, int period, int pay) { this(name, period, 1, pay, 0); }
+	
+	private void setName(String name) { this.name = name; }
+	private void setPeriod(int period) {
+		switch(period) {
+		case 1, 2, 3, 5, 10:
+			this.period = period;
+		}
+	}
+	private void setCount(int count) { this.count = count; }
+	private int getCount() { return period * 12 + 1; }
+	private void setPay(int pay) {
+		if (pay % 10000 == 0)
+		this.pay = pay;
+	}
+	private void setBalance(int balance) {
+		if (balance < 0) return;
+		this.balance = balance;
+	}
+	private String getFull() {
+		if (count == getCount()) return "현재상태: 만기\n";
+		else return "";
+	}
+	
+	public void show() {
+		while (true) {
+			System.out.println("이름: " + name);
+			System.out.println("기간: " + period);
+			System.out.println("납입회차: " + count);
+			System.out.println("납입금액: " + pay);
+			System.out.println("잔액: " + balance);
+			System.out.print(getFull());
+			System.out.println("만기 예상 금액: " + getFullBalance() + "\n");
+			Scanner sc = new Scanner(System.in);
+			System.out.print("next를 호출하고 싶으면 1을, 종료하고 싶으면 아무키 입력");
+			int num = sc.nextInt();
+			if (num == 1) {
+				setNext();
+				System.out.println("<next 진행 후 출력>");
+			}
+			else break;
+		}
+	}
+	private int getFullBalance() { return (getCount() - count) * pay + balance; }
+	private void setNext() {
+		if (count != getCount()) {
+			count++;
+			balance += pay;
+		}
+	}
+}
---
+++ day11/src/oop/modifier2/Test01.java
@@ -0,0 +1,14 @@
+package oop.modifier2;
+
+public class Test01 {
+	public static void main(String[] args) {
+		Bankbook b1 = new Bankbook("유재석", 3, 15, 1000000, 15000000);
+		Bankbook b2 = new Bankbook("강호동", 2, 5, 500000, 2500000);
+		Bankbook b3 = new Bankbook("신동엽", 2, 8, 800000, 10000000);
+		
+		System.out.println("<KH 내집마련 적금통장 가입자 정보>");
+		b1.show();
+		b2.show();
+		b3.show();
+	}
+}
