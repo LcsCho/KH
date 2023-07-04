fc16e43440490b66260ad57f9dcaa09eb6bdb3d9
jhcho <jhcho@LenovoIDEAPAD>
2023-07-04 13:55:59 +0900

    day12

--- day12/src/oop/keyword4/Robot.java
+++ day12/src/oop/keyword4/Robot.java
@@ -1,10 +1,12 @@
 package oop.keyword4;
 
+import java.util.Random;
+
 public class Robot {
 	public static int square(int a) { return a * a; }
 	public static double triangle(double width, double height) { return width * height / 2; }
 	public static double circle(int radius) { return radius * radius * 3.14f; }
-	public static int[] sort(int arr[]) {
+	public static void sort(int arr[]) {
 		for (int i = 0; i < arr.length - 1; i++) {
 			for (int j = i + 1; j < arr.length; j++) {
 				if (arr[i] > arr[j]) {
@@ -14,6 +16,35 @@ public class Robot {
 				}
 			}
 		}
-		return arr;
+	}
+	public static void reverseSort(int arr[]) {
+		int left = 0;
+		int right = arr.length - 1;
+		
+		for(int i=0; i < arr.length/2; i++) {
+			int backup = arr[left];
+			arr[left] = arr[right];
+			arr[right] = backup;
+			
+			left++;
+			right--;
+		}
+	}
+	
+	public static void shuffle(int arr[]) {
+		Random r = new Random();
+		int count = 0;
+		int countAll = 0;
+		for (int i = 0; i < arr.length; i++) {
+			countAll++;
+			int mix = r.nextInt(arr.length);
+			if (mix == i && i >= 1) { // i가 0일 때 -1이되면 에러가 생기므로 i이가 1이상일 때만 i--를 진행하여 횟수를 늘림
+				count++;
+				i--; 
+			}
+			int temp = arr[i];
+			arr[i] = arr[mix];
+			arr[mix] = temp;	
+		}
 	}
 }
--- day12/src/oop/keyword4/Test01.java
+++ day12/src/oop/keyword4/Test01.java
@@ -7,9 +7,18 @@ public class Test01 {
 		System.out.println(r.square(11));
 		System.out.println(r.triangle(3, 7));
 		System.out.println(r.circle(5));
-		int[] arr = {50, 30, 20, 10, 40, 80};
-		arr = r.sort(arr);
+		
+		// (주의) 참조형 데이터는 리모컨만 전달하면 외부에서 직접 제어 가능
+		int[] arr = {10, 20, 30, 40, 50};
+		r.sort(arr); // 리모컨 전달(call-by-reference)
 		for (int i = 0; i < arr.length; i++) System.out.print(arr[i] + " ");
+		System.out.println();
+		
+		r.reverseSort(arr);
+		for (int i : arr) System.out.print(i + " ");
+		System.out.println();
+		
+		r.shuffle(arr);
 		for (int i : arr) System.out.print(i + " ");
 	}
 }
