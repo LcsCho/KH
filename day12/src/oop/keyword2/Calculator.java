05bad6e10faf2de639c14210771323890337ba0e
jhcho <jhcho@LenovoIDEAPAD>
2023-07-04 11:03:33 +0900

    day12

--- day12/src/oop/keyword2/Calculator.java
+++ day12/src/oop/keyword2/Calculator.java
@@ -1,17 +1,5 @@
 package oop.keyword2;
 
 public class Calculator {
-	// 필드
-	private int left, right;
-	
-	// setter+getter 생략
-	public int getTot() {
-		return this.left + this.right;
-	}
-	
-	// 생성자
-	public Calculator(int left, int right) {
-		this.left = left;
-		this.right = right;
-	}
+
 }	
--- day12/src/oop/keyword2/Test01.java
+++ day12/src/oop/keyword2/Test01.java
@@ -2,9 +2,6 @@ package oop.keyword2;
 
 public class Test01 {
 	public static void main(String[] args) {
-		// 10 + 20을 객체지향 방식으로 처리
-		Calculator a = new Calculator(10, 20);
-		int result = a.getTot();
-		System.out.println(result);
+
 	}
 }
