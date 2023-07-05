cc60adf04639872632da7d5b5567226e880a0889
jhcho <jhcho@LenovoIDEAPAD>
2023-07-05 16:55:15 +0900

    day13

--- day13/src/oop/inherit8/Character.java
+++ day13/src/oop/inherit8/Character.java
@@ -18,5 +18,6 @@ public abstract class Character {
 	public abstract void shop();
 	public void show() { 
 		System.out.println("<캐릭터 정보>");
-		System.out.println("아이디: " + this.getId() + ", 레벨: " + this.getLv()); }
+		System.out.println("아이디: " + this.getId() + ", 레벨: " + this.getLv()); 
+	}
 }
