590fbc31ffc8514666e8aa9a8c527a54bba71fbe
jhcho <jhcho@LenovoIDEAPAD>
2023-07-05 15:50:41 +0900

    day13

--- day13/src/oop/inherit6/Galaxy23s.java
+++ day13/src/oop/inherit6/Galaxy23s.java
@@ -5,6 +5,7 @@ public final class Galaxy23s extends Galaxy {
 	public Galaxy23s(String number, String color) {
 		super(number, color);
 	}
+	// 메서드 재정의
 	public void call() { System.out.println("갤럭시23s 전화기능 실행"); }
 	public void sms() { System.out.println("갤럭시23s 문자 기능 실행"); }
 	public void samsungPay() { System.out.println("갤럭시23s 삼성페이 기능 실행"); }
