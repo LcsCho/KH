7b15aae8302d80e95d532795b5ca4fe74f575457
jhcho <jhcho@LenovoIDEAPAD>
2023-07-05 15:27:24 +0900

    day13

--- day13/src/oop/inherit6/IPhone13.java
+++ day13/src/oop/inherit6/IPhone13.java
@@ -1,8 +1,8 @@
 package oop.inherit6;
 
 public class IPhone13 extends IPhone {
-	public void call() { System.out.println("IPhone13 전화 기능 실행"); }
-	public void sms() { System.out.println("IPhone13 문자 기능 실행"); }
-	public void itunes() { System.out.println("IPhone13 아이튠즈 기능 실행"); }
-	public void siri() { System.out.println("IPhone13 음성인식 기능 실행"); }
+	public void call() { System.out.println("아이폰13 전화 기능 실행"); }
+	public void sms() { System.out.println("아이폰13 문자 기능 실행"); }
+	public void itunes() { System.out.println("아이폰13 아이튠즈 기능 실행"); }
+	public void siri() { System.out.println("아이폰13 음성인식 기능 실행"); }
 }
--- day13/src/oop/inherit6/IPhone14.java
+++ day13/src/oop/inherit6/IPhone14.java
@@ -1,8 +1,8 @@
 package oop.inherit6;
 
 public class IPhone14 extends IPhone {
-	public void call() { System.out.println("IPhone14 전화 기능 실행"); }
-	public void sms() { System.out.println("IPhone14 문자 기능 실행"); }
-	public void itunes() { System.out.println("IPhone14 아이튠즈 기능 실행"); }
-	public void siri() { System.out.println("IPhone14 음성인식 기능 실행"); }
+	public void call() { System.out.println("아이폰14 전화 기능 실행"); }
+	public void sms() { System.out.println("아이폰14 문자 기능 실행"); }
+	public void itunes() { System.out.println("아이폰14 아이튠즈 기능 실행"); }
+	public void siri() { System.out.println("아이폰14 음성인식 기능 실행"); }
 }
