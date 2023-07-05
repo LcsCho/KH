41c19e860c5e946843bdb34209a77f144e925252
jhcho <jhcho@LenovoIDEAPAD>
2023-07-05 17:20:25 +0900

    day13

--- day13/src/oop/inherit8/Test01.java
+++ day13/src/oop/inherit8/Test01.java
@@ -2,19 +2,19 @@ package oop.inherit8;
 
 public class Test01 {
 	public static void main(String[] args) {
-		Warrior w = new Warrior("전사");
+		Warrior w = new Warrior("도도");
 		w.show();
 		w.attack();
 		w.move();
 		w.shop();
 		
-		Magician m = new Magician("마법사");
+		Magician m = new Magician("델리키");
 		m.show();
 		m.attack();
 		m.move();
 		m.shop();
 		
-		Archer a = new Archer("궁수");
+		Archer a = new Archer("바우");
 		a.show();
 		a.attack();
 		a.move();
