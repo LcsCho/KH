df298d376f207fd3d274846c84c9f63394a89054
jhcho <jhcho@LenovoIDEAPAD>
2023-07-05 10:55:26 +0900

    day13

--- day13/src/oop/inherit3/Test01.java
+++ day13/src/oop/inherit3/Test01.java
@@ -2,21 +2,20 @@ package oop.inherit3;
 
 public class Test01 {
 	public static void main(String[] args) {
-		Chrome c = new Chrome();
-		Edge e = new Edge();
-		Whale w = new Whale();
-		
+		Chrome c = new Chrome();		
 		c.url();
 		c.refresh();
 		c.move();
 		c.develop();
 		c.chromeStore();
 		
+		Edge e = new Edge();
 		e.url();
 		e.refresh();
 		e.move();
 		e.fullScreen();
 		
+		Whale w = new Whale();
 		w.url();
 		w.refresh();
 		w.move();
