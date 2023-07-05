a3d6803d0158bcc760f2aac75adadb41d4d0e747
jhcho <jhcho@LenovoIDEAPAD>
2023-07-05 12:53:02 +0900

    day13

--- day13/src/oop/inherit4/Mp4.java
+++ day13/src/oop/inherit4/Mp4.java
@@ -1,6 +1,6 @@
 package oop.inherit4;
 
-public class Mp4 extends File {
+public class Mp4 extends Mp3 {
 	private int speed;
 
 	public int getSpeed() {
@@ -9,12 +9,6 @@ public class Mp4 extends File {
 
 	public void setSpeed(int speed) {
 		this.speed = speed;
-	}
-	public void forward() {
-		System.out.println("빨리감기");
-	}
-	public void rewind() {
-		System.out.println("되감기");
 	}
 	
 }
--- day13/src/oop/inherit4/Ppt.java
+++ day13/src/oop/inherit4/Ppt.java
@@ -1,13 +1,6 @@
 package oop.inherit4;
 
-public class Ppt extends File {
-	private int pagesize;
-	public int getPagesize() {
-		return pagesize;
-	}
-	public void setPagesize(int pagesize) {
-		this.pagesize = pagesize;
-	}
+public class Ppt extends Hwp {
 	public void slideShow() {
 		System.out.println("슬라이드쇼");
 	}
