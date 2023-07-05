fc6270b4a240105f3f9be630b25e2bee424699e6
jhcho <jhcho@LenovoIDEAPAD>
2023-07-05 11:03:54 +0900

    day13

--- day13/src/oop/inherit3/Browser.java
+++ day13/src/oop/inherit3/Browser.java
@@ -4,7 +4,7 @@ public class Browser {
 	private String url;
 	public String getUrl() { return url; }
 	public void setUrl(String url) { this.url = url; }
-	public void url() { System.out.println("주소 저장 실행"); }
+	public void url() { System.out.println(getUrl()); }
 	public void refresh() { System.out.println("새로고침 실행"); }
 	public void move() { System.out.println("페이지 이동 실행"); }
 }
--- day13/src/oop/inherit3/Test01.java
+++ day13/src/oop/inherit3/Test01.java
@@ -2,7 +2,8 @@ package oop.inherit3;
 
 public class Test01 {
 	public static void main(String[] args) {
-		Chrome c = new Chrome();		
+		Chrome c = new Chrome();	
+		c.setUrl("https://www.google.com");
 		c.url();
 		c.refresh();
 		c.move();
@@ -10,12 +11,14 @@ public class Test01 {
 		c.chromeStore();
 		
 		Edge e = new Edge();
+		e.setUrl("https://www.google.com");
 		e.url();
 		e.refresh();
 		e.move();
 		e.fullScreen();
 		
 		Whale w = new Whale();
+		w.setUrl("https://www.google.com");
 		w.url();
 		w.refresh();
 		w.move();
