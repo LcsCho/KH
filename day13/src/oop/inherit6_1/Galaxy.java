5232b6566dcfc8187a74d5c222ef4217c0f120a1
jhcho <jhcho@LenovoIDEAPAD>
2023-07-05 15:45:26 +0900

    day13

--- day13/src/oop/inherit6_1/Galaxy.java
+++ day13/src/oop/inherit6_1/Galaxy.java
@@ -4,5 +4,9 @@ public class Galaxy extends Mobile {
 	public Galaxy(String number, String color) {
 		super(number, color);
 	}
+	
+	public void samsungPay() {
+		System.out.println("삼성페이 기능");
+	}
 
 }
