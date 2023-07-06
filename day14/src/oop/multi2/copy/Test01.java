c1927797670b3bedc75a26f394650695ebb03b54
jhcho <jhcho@LenovoIDEAPAD>
2023-07-06 13:38:47 +0900

    day14

--- day14/src/oop/multi2/copy/Test01.java
+++ day14/src/oop/multi2/copy/Test01.java
@@ -8,22 +8,32 @@ public class Test01 {
 		Bus b = new Bus();
 		Kickboard k = new Kickboard();
 		
+		System.out.println("<드론>");
 		d.on();
 		d.off();
 		d.move();
 		d.fly();
+		System.out.println();
 		
+		System.out.println("<비행기>");
 		a.move();
 		a.fly();
 		a.reserve();
+		System.out.println();
 		
+		System.out.println("<기차>");
 		t.move();
 		t.reserve();
+		System.out.println();
 		
+		System.out.println("<버스>");
 		b.move();
+		System.out.println();
 		
+		System.out.println("<전동킥보드>");
 		k.on();
 		k.off();
-		k.move();	
+		k.move();
+		System.out.println();
 	}
 }
