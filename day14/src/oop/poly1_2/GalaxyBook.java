8c85788e292c4d42ba8979930f8dbddeb229cb74
jhcho <jhcho@LenovoIDEAPAD>
2023-07-06 11:02:52 +0900

    day14

--- day14/src/oop/poly1_2/GalaxyBook.java
+++ day14/src/oop/poly1_2/GalaxyBook.java
@@ -1,8 +1,12 @@
 package oop.poly1_2;
 
 public class GalaxyBook extends Laptop {
-	public void show() {
-		System.out.print("갤럭시 북으로 ");
-		
-	}
+	@Override
+	public void power() { System.out.println("갤럭시북 전원 기능 테스트"); }
+	
+	@Override
+	public void video() { System.out.println("갤럭시북 영상재생 기능 테스트"); }
+
+	@Override
+	public void typing() { System.out.println("갤럭시북 타이핑 기능 테스트"); }
 }
--- day14/src/oop/poly1_2/MacBook.java
+++ day14/src/oop/poly1_2/MacBook.java
@@ -1,8 +1,12 @@
 package oop.poly1_2;
 
 public class MacBook extends Laptop{
-	public void show() {
-		System.out.print("맥북으로 ");
-		
-	}
+	@Override
+	public void power() { System.out.println("맥북 전원 기능 테스트"); }
+	
+	@Override
+	public void video() { System.out.println("맥북 영상재생 기능 테스트"); }
+
+	@Override
+	public void typing() { System.out.println("맥북 타이핑 기능 테스트"); }
 }
