41bab32b5fa23acc15299ec078b89a079baf3556
jhcho <jhcho@LenovoIDEAPAD>
2023-07-06 16:04:07 +0900

    day14

--- day14/src/api/lang/test1/Test01.java
+++ day14/src/api/lang/test1/Test01.java
@@ -2,9 +2,13 @@ package api.lang.test1;
 
 public class Test01 {
 	public static void main(String[] args) {
+		// StringBuffer 클래스를 문서만 보고 써보기
+		
+		// 1. 객체 생성
 		StringBuffer a = new StringBuffer(10);
 		StringBuffer b = new StringBuffer("hello");
 		
+		// 2. 메서드 활용
 		a.append("KH정보교육원");
 		System.out.println("a요약본 = " + a.toString());
 		System.out.println("b요약본 = " +b.toString());
