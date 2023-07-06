f0087b72b595585855107f9d21de2675fe2e2dc2
jhcho <jhcho@LenovoIDEAPAD>
2023-07-06 10:03:57 +0900

    day13

--- day13/src/oop/inherit8/Test02.java
+++ day13/src/oop/inherit8/Test02.java
@@ -2,13 +2,16 @@ package oop.inherit8;
 
 public class Test02 {
 	public static void main(String[] args) {
-		Warrior a = new Warrior("테스터1");
-//		Warrior b = new Warrior("테스터2");
-		// Magician b = new Magician("테스터2");
+		// 객체를 업캐스팅하여 생성
+		// = 상속 관계에서 특정 클래스로 대충 묶어서 표현하고 싶을 때
 		
-	//	Warrior b = new Warrior("테스터");
+		Warrior a = new Warrior("테스터1");
+	    // Warrior b = new Warrior("테스터2");
+	    // Magician b = new Magician("테스터2");
+		
+	    // Warrior b = new Warrior("테스터");
 		// Magician b = new Magician("테스터2");
-		 Archer b = new Archer("테스터2");
+		Archer b = new Archer("테스터2");
 		
 		
 		
