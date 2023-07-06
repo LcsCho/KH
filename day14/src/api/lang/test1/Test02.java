6eeda1e5972f1825e74908c8545448f04977e459
jhcho <jhcho@LenovoIDEAPAD>
2023-07-06 16:43:34 +0900

    day14

--- day14/src/api/lang/test1/Test02.java
+++ day14/src/api/lang/test1/Test02.java
@@ -2,13 +2,12 @@ package api.lang.test1;
 
 import java.util.Properties;
 
-
 public class Test02 {
 	public static void main(String[] args) {
-		// 1.
+		// 1. 객체 생성
 		Properties prop = new Properties();
 		
-		// 2.
+		// 2. 메서드 활용
 		prop.setProperty("hello", "world");
 		prop.setProperty("good", "bye");
 		System.out.println(prop.toString());
