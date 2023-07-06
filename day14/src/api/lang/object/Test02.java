89fe9e462fe48307ff689ac21e3a9d1b9ddcc11b
jhcho <jhcho@LenovoIDEAPAD>
2023-07-06 15:12:13 +0900

    day14

---
+++ day14/src/api/lang/object/Test02.java
@@ -0,0 +1,11 @@
+package api.lang.object;
+
+public class Test02 {
+	public static void main(String[] args) {
+		// Object는 모든 클래스의 시작(조상)이다에 대한 검증
+		
+		// Object a = new Object();
+		Object a = 10;
+		Object b = "hello";
+	}
+}
