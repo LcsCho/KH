18115584987af387eb931e3e47ed3d4c3199c0ac
jhcho <jhcho@LenovoIDEAPAD>
2023-07-06 10:47:40 +0900

    day13

--- day13/src/oop/inherit8/copy/Fightable.java
+++
@@ -1,9 +0,0 @@
-package oop.inherit8.copy;
-
-// 두 개의 캐릭터로 서로간의 상호작용을 하도록 처리하는 클래스
-// - ex: 거래, 전투, ...
-
-public interface Fightable { // Archer, Magician, Warrior 클래스에 implements Fightable을 추가함
-    void attack();
-}
-
