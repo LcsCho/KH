81ca6b158de6a9aecabfe875d5f8b03f5f51932f
jhcho <jhcho@LenovoIDEAPAD>
2023-07-06 10:51:24 +0900

    day14

---
+++ day14/src/oop/poly1_2/Laptop.java
@@ -0,0 +1,16 @@
+package oop.poly1_2;
+
+public class Laptop {
+	public void power() {
+		System.out.println("전원을 켭니다.");
+	}
+	
+	public void video() {
+		System.out.println("동영상을 재생합니다.");
+	}
+	
+	public void typing() {
+		System.out.println("타이핑을 실행합니다.");
+		
+	}
+}
---
+++ day14/src/oop/poly1_2/MacBook.java
@@ -0,0 +1,8 @@
+package oop.poly1_2;
+
+public class MacBook extends Laptop{
+	public void show() {
+		System.out.print("맥북으로 ");
+		
+	}
+}
