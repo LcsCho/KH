3dfab046d9ce8660785a14c5c6f461d6240a8555
jhcho <jhcho@LenovoIDEAPAD>
2023-07-06 17:50:40 +0900

    day14

--- day14/src/api/lang/string/Test02.java
+++ day14/src/api/lang/string/Test02.java
@@ -6,16 +6,13 @@ public class Test02 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		
-		String id = "khacademy";
-		String pw = "student1234";
-
 		System.out.print("아이디 입력: ");
-		String inputId = sc.next();
+		String id = sc.next();
 		System.out.print("비밀번호 입력: ");
-		String inputPw = sc.next();
+		String pw = sc.next();
 		sc.close();
 		
-		boolean isLogin = id.equalsIgnoreCase(inputId) && pw.equals(inputPw);
+		boolean isLogin = id.equalsIgnoreCase("khacademy") && pw.equals("student1234");
 		if (isLogin) System.out.println("로그인 성공");
 		else System.out.println("로그인 실패");
 	}
