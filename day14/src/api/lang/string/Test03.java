84482a8e8f9ad66ca4d91afcf0308218d29091b7
jhcho <jhcho@LenovoIDEAPAD>
2023-07-06 18:01:36 +0900

    day14

--- day14/src/api/lang/string/Test03.java
+++ day14/src/api/lang/string/Test03.java
@@ -2,6 +2,12 @@ package api.lang.string;
 
 public class Test03 {
 	public static void main(String[] args) {
+		// 문자열의 구조 / 비교 원리
+		// = 자바에서 문자열은 불변(immutable)으로 관리
 		
+		String a = "hello";
+		String b = "hello";
+		String c = new String("hello");
+		String d = new String("hello");
 	}
 }
