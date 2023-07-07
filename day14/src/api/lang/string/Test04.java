8e9f2e7f7aae6deaeeb495b85a84b7eea374bf61
jhcho <jhcho@LenovoIDEAPAD>
2023-07-07 09:48:07 +0900

    day14

--- day14/src/api/lang/string/Test04.java
+++ day14/src/api/lang/string/Test04.java
@@ -10,8 +10,8 @@ public class Test04 {
 		sc.close();
 		boolean isNickname1 = !nickname.contains("운영자");
 		boolean isNickname2 = nickname.length() >= 2 && nickname.length() <= 10;
-		if(!(isNickname1)) System.out.println("운영자라는 단어를 포함할 수 없음!");
-		else if (!(isNickname2)) System.out.println("2글자 이상 10글자 이하로만 작성!");
+		if (!(isNickname2)) System.out.println("2글자 이상 10글자 이하로만 작성!");
+		else if(!(isNickname1)) System.out.println("운영자라는 단어를 포함할 수 없음!");
 		else System.out.println("닉네임 설정이 완료되었습니다.");
 	}
 }
