e68d317bbd80d9f3bd7815dbef6dc6bf543dc21d
jhcho <jhcho@LenovoIDEAPAD>
2023-07-07 16:19:37 +0900

    day15

--- day15/src/api/lang/string2/Test04.java
+++ day15/src/api/lang/string2/Test04.java
@@ -10,10 +10,10 @@ public class Test04 {
 		String id = sc.next();
 		sc.close();
 		
-		String regex = "^[a-z][_0-9a-z-]{4,19}$";
+		String regex = "^[a-z][a-z0-9\\-_]{4,19}$";
 		boolean isValid = id.matches(regex);
 		
-		if (isValid) System.out.println("아이디를 정상적으로 입력함");
-		else System.out.println("아이디를 비정상적으로 입력함");
+		if (isValid) System.out.println("아이디를 정상적으로 입력했습니다.");
+		else System.out.println("5~20자의 영문 소문자, 숫자와 특수기호(_), (-)만 사용할 수 있습니다.");
 	}
 }
---
+++ day15/src/api/lang/string2/Test05_1.java
@@ -0,0 +1,15 @@
+package api.lang.string2;
+
+public class Test05_1 {
+	public static void main(String[] args) {
+		String birthDate = "1998-06-22";
+		
+		
+		String regex = "^(19|20)[0-9]{2}-(0[13578]-(0[1-9]|2[0-9]|3[01])|1[02]-"
+				+ "(0[1-9]|2[0-9]|3[01])|02-(0[1-9]|2[0-9])|0[2469]"
+				+ "-(0[1-9]|2[0-9]|30)|11-(0[1-9]|2[0-9]|30))$";
+		boolean isValue = birthDate.matches(regex);
+		if (isValue) System.out.println("생년월일을 정상적으로 입력함");
+		else System.out.println("생년월일을 비정상적으로 입력함");
+	}
+}
