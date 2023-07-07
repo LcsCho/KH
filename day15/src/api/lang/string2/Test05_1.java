668d7c186fd45c31c8603af9e93bd5b3ba71ac5f
jhcho <jhcho@LenovoIDEAPAD>
2023-07-07 17:06:47 +0900

    day15

--- day15/src/api/lang/string2/Test05_1.java
+++ day15/src/api/lang/string2/Test05_1.java
@@ -2,10 +2,10 @@ package api.lang.string2;
 
 public class Test05_1 {
     public static void main(String[] args) {
-        String birthDate = "1998-06-22";
+        String birthDate = "2020-06-22";
 
-        String[] dateParts = birthDate.split("-");
-        int year = Integer.parseInt(dateParts[0]);
+        String yearStr = birthDate.substring(0, 4);
+        int year = Integer.parseInt(yearStr);
 
         boolean isLeapYear = false;
         if (year % 4 == 0) {
@@ -13,14 +13,10 @@ public class Test05_1 {
                 isLeapYear = true;
             }
         }
-
-        String regex;
-        if (isLeapYear) {
-            regex = "^(19|20)[0-9]{2}-(0[13578]-(0[1-9]|2[0-9]|3[01])|1[02]-(0[1-9]|2[0-9]|3[01])|02-(0[1-9]|1[0-9]|2[0-9])|0[469]-(0[1-9]|2[0-9]|30)|11-(0[1-9]|2[0-9]|30))$";
-        } else {
-            regex = "^(19|20)[0-9]{2}-(0[13578]-(0[1-9]|2[0-9]|3[01])|1[02]-(0[1-9]|2[0-9]|3[01])|02-(0[1-9]|1[0-9]|2[0-8])|0[469]-(0[1-9]|2[0-9]|30)|11-(0[1-9]|2[0-9]|30))$";
-        }
-
+        
+        String regex = "";
+        if (isLeapYear) regex = "^(19|20)[0-9]{2}-(0[13578]-(0[1-9]|2[0-9]|3[01])|1[02]-(0[1-9]|2[0-9]|3[01])|02-(0[1-9]|1[0-9]|2[0-9])|0[469]-(0[1-9]|2[0-9]|30)|11-(0[1-9]|2[0-9]|30))$";
+        
         boolean isValue = birthDate.matches(regex);
         if (isValue) {
             System.out.println("생년월일을 정상적으로 입력함");
