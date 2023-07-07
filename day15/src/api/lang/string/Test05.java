ac501d6c2813cf8d4a441b5a0bff3c2f502b184c
jhcho <jhcho@LenovoIDEAPAD>
2023-07-07 09:56:09 +0900

    day15

--- day15/src/api/lang/string/Test05.java
+++ day15/src/api/lang/string/Test05.java
@@ -1,5 +1,11 @@
 package api.lang.string;
 
 public class Test05 {
-
+	public static void main(String[] args) {
+		// 문자열 검사
+		// = 전부 또는 일부분에 대한 검사를 위한 명령
+		
+		String nickname = "운영자입니다";
+		System.out.println(nickname.contains("운영자"));
+	}
 }
