981098373ea776107a6bdb4c735480dcd2c2608b
jhcho <jhcho@LenovoIDEAPAD>
2023-07-07 13:40:08 +0900

    day15

--- day15/src/api/lang/string/Test08.java
+++ day15/src/api/lang/string/Test08.java
@@ -9,6 +9,7 @@ public class Test08 {
 		
 		System.out.print("문장을 입력: ");
 		String line = sc.nextLine();
+		sc.close();
 		
 		String[] slangArr = new String[] {
 			"수박씨", "신발끈", "개나리", "십장생", "시베리아", "주옥", "조카", "게불", "10원"
@@ -16,6 +17,5 @@ public class Test08 {
 		
 		for (String slang : slangArr) { line = line.replace(slang, "*".repeat(slang.length())); }
         System.out.println(line);
-		sc.close();
 	}
 }
