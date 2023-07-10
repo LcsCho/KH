5dd80152954b7df85286ffbb493202e2580b2a4f
jhcho <jhcho@LenovoIDEAPAD>
2023-07-10 20:17:24 +0900

    day16

--- day16/src/api/exception/Test05.java
+++ day16/src/api/exception/Test05.java
@@ -12,13 +12,16 @@ public class Test05 {
 			System.out.print("금액: ");
 			int money = sc.nextInt();
 			if  (money < 0) { // 자바가 인식하지 못하는 문제 상황
-				
+				// throw를 이용하면 실행을 중지하고 catch블록으로 이동한다.
+				// Exception ex = new Exception();
+				// throw ex;
+				throw new Exception("금액이 음수일 수 없습니다.");
 			}
 			
 			System.out.print("인원: ");
 			int people = sc.nextInt();
 			if (people < 0) { // 자바가 인식하지 못하는 문제 상황
-				
+				throw new Exception("인원은 음수일 수 없습니다.");
 			}
 			
 			sc.close();
@@ -29,8 +32,18 @@ public class Test05 {
 			System.out.println(remain + "원은 저희가 지원해 드려요!");
 		} 
 		 catch (Exception e){ // 필살기 예외처리 
-			// 플랜B. 입력을 잘못했을 때
-			System.err.println("프로그램 오류 발생");
+			// System.err.println("프로그램 오류 발생");
+			 
+			// 합쳐놓고 보니 자세한 정보가 그립더라
+			// System.out.println(e);
+			// System.out.println(e instanceof ArithmeticException);
+			 
+			// 예외 객체에 설정된 메세지가 있다면 출력할 수 있다.
+			 if (e.getMessage() == null) 
+				 System.err.println("오류 발생");
+			 
+			 else 
+				 System.out.println(e.getMessage());
 		}
 	}
 }
---
+++ day16/src/api/exception/Test06.java
@@ -0,0 +1,42 @@
+package api.exception;
+
+import java.time.LocalDate;
+import java.util.Scanner;
+
+public class Test06 {
+	public static void main(String[] args) {
+		try {
+			Scanner sc = new Scanner(System.in);
+
+			System.out.print("생년월일 입력(YYYY-MM-DD): ");
+			String birth = sc.next();
+			sc.close();
+			LocalDate now = LocalDate.now();
+
+			int year = Integer.parseInt(birth.substring(0, 4));
+			if (year < 1900) {
+				throw new Exception("연도를 1900보다 크게 입력하세요.\n");
+			}
+			
+			int month = Integer.parseInt(birth.substring(5, 7));
+			if (month < 1 || month > 12) {
+				throw new Exception("월을 01~12로 입력하세요.\n");
+			}
+			
+			int day = Integer.parseInt(birth.substring(8, 10));
+			boolean isFuture = now.getYear() <= year && now.getMonthValue() <= month && now.getDayOfMonth() < day;
+			if (isFuture) {
+				throw new Exception("현재 이전의 날짜만 입력하세요.\n");
+			}
+			boolean isAgeMinus = now.getMonthValue() <= month && now.getDayOfMonth() < day;
+			
+			int age = now.getYear() - year; {
+				if (isAgeMinus) age--;
+			}
+			System.out.println("만나이: " + age);
+
+		} catch (Exception e) {
+			System.out.println("형식에 맞게 '-'을 포함하여 10자리만 입력하시거나, 범위에 맞게 자연수만 입력하세요");
+		}
+	}
+}
