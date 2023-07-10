6d1d74c4e4b0acc240d94bc773c15118efced642
jhcho <jhcho@LenovoIDEAPAD>
2023-07-10 12:41:37 +0900

    day16

--- day16/src/api/lang/etc/Test01.java
+++ day16/src/api/lang/etc/Test01.java
@@ -5,5 +5,14 @@ public class Test01 {
 		// 생성자가 없는 클래스
 		// = 클래스는 객체 생성이 주 목적
 		// = 생성자가 없다는 것은 생성자가 private이란 소리이다.
+		// = 생성자가 없다는 것은 객체를 만들 수 없다는 의미
+
+		// 1. 객체를 아예 못만들게 하려는 목적
+		// 2. 객체를 만들어주는 메서드를 제공하려는 목적(통제)
+		
+		// Math a = new Math();
+		// System b = new System();
+		// Runtime c = new Runtime();
+		// Integer d = new Integer(10);
 	}
 }
---
+++ day16/src/api/lang/etc/Test02.java
@@ -0,0 +1,8 @@
+package api.lang.etc;
+
+public class Test02 {
+	public static void main(String[] args) {
+		// Math 클래스
+		
+	}
+}
