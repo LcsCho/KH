bd7750d6b18b1f35488c42a54d031172dd1abda0
jhcho <jhcho@LenovoIDEAPAD>
2023-07-10 14:48:08 +0900

    day16

--- day16/src/api/lang/etc/Test05.java
+++ day16/src/api/lang/etc/Test05.java
@@ -3,9 +3,9 @@ package api.lang.etc;
 public class Test05 {
 	public static void main(String[] args) {
 		// Integer 클래스
-		// = Wrapper 클래스
+		// = Wrapper 클래스(총 8종류) 
 		// = int와 관련된 여러 기능들을 제공하는 클래스
-		// = int를 객체 혀태로 사용하도록 만들어주는 클래스
+		// = int를 객체 형태로 사용하도록 만들어주는 클래스
 		
 		// 생성 - Java 9 이후부터 비추천 처리
 		Integer a = new Integer(10);
@@ -13,9 +13,23 @@ public class Test05 {
 		System.out.println(a.toString());
 		System.out.println(b.toString());
 		
+		// 자주 쓰는 값은(-128~+127)은 신규로 만들지 않고 캐싱된 값을 사용
 		Integer c = Integer.valueOf(10);
 		Integer d = Integer.valueOf("10");
 		System.out.println("c = " + c);
 		System.out.println("d = " + d);
+		
+		// int와 Integer는 무슨 차이가 있는가?
+		// - 10 + 20을 각각의 방식으로 계산
+		// - 123을 각각의 방식으로 16진수로 변환하여 출력
+		
+		int v1 = 10, v2 = 20;
+		int v3 = v1 + v2;
+		System.out.println("v3 = " + v3);
+		
+		Integer w1 = Integer.valueOf(10);
+		Integer w2 = Integer.valueOf(20);
+		Integer w3 = Integer.sum(w1, w2);
+		System.out.println("w3 = " + w3);
 	}
 }
