0fa87a1b1770e658b9c3da9fa963cd20980f535a
jhcho <jhcho@LenovoIDEAPAD>
2023-07-10 12:38:20 +0900

    day16

--- day16/src/api/lang/etc/Test01.java
+++ day16/src/api/lang/etc/Test01.java
@@ -2,6 +2,8 @@ package api.lang.etc;
 
 public class Test01 {
 	public static void main(String[] args) {
-		
+		// 생성자가 없는 클래스
+		// = 클래스는 객체 생성이 주 목적
+		// = 생성자가 없다는 것은 생성자가 private이란 소리이다.
 	}
 }
