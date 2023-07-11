83ec7d761209149d7604fce872db7c2e8e51a162
jhcho <jhcho@LenovoIDEAPAD>
2023-07-11 14:40:32 +0900

    day17

--- day17/src/api/time/Test05.java
+++ day17/src/api/time/Test05.java
@@ -1,17 +1,22 @@
 package api.time;
 
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
+import java.util.Date;
 import java.util.Random;
 
 public class Test05 {
 	public static void main(String[] args) {
 		Calendar c = Calendar.getInstance();
 		Random r = new Random();
-		int day = r.nextInt(365) + 1;
 		
+		// 계산
+		int day = r.nextInt(365) + 1;
 		c.set(2023, 0, day);
 		
-		System.out.println(c.get(Calendar.YEAR) + "년 " + (c.get(Calendar.MONTH) + 1) + "월 " + c.get(Calendar.DAY_OF_MONTH) + "일");
+		Date d = c.getTime();
+		SimpleDateFormat fmt = new SimpleDateFormat("yyyy-MM-dd E");
+		System.out.println(fmt.format(d));
 		
 	}
 }
--- day17/src/api/time/Test06.java
+++ day17/src/api/time/Test06.java
@@ -1,15 +1,17 @@
 package api.time;
 
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
+import java.util.Date;
 
 public class Test06 {
 	public static void main(String[] args) {
 		Calendar c = Calendar.getInstance();
 		
-		c.set(c.get(Calendar.YEAR), c.get(Calendar.MONTH) + 1, c.get(Calendar.DAY_OF_MONTH) + 100);
+		c.set(c.get(Calendar.YEAR), c.get(Calendar.MONTH), c.get(Calendar.DAY_OF_MONTH) + 100);
 		
-		System.out.print(c.get(Calendar.YEAR) + "년 ");
-		System.out.print(c.get(Calendar.MONTH) + "월 ");
-		System.out.println(c.get(Calendar.DAY_OF_MONTH) + "일");
+		Date d = c.getTime();
+		SimpleDateFormat fmt = new SimpleDateFormat("yyyy-MM-dd E");
+		System.out.println(fmt.format(d));
 	}
 }
