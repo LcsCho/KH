534e7715842a184a7b058db34a3973a38fd15837
jhcho <jhcho@LenovoIDEAPAD>
2023-07-11 14:04:02 +0900

    day17

--- day17/src/api/time/Test04.java
+++ day17/src/api/time/Test04.java
@@ -1,8 +1,40 @@
 package api.time;
 
+import java.util.Calendar;
+
 public class Test04 {
 	public static void main(String[] args) {
 		// Calendar의 장점
-		
+		// = 1900년이 기준이 아니기 때문에 일반적인 연도 설정 가능
+
+		Calendar c = Calendar.getInstance();
+
+		// 항목별로 설정
+		// c.set(Calendar.YEAR, 2000);
+		// c.set(Calendar.MONTH, 0); // 1월이 0부터 시작(주의)
+		// c.set(Calendar.DATE, 1);
+		// c.set(2000, 0, 1);
+		c.set(2000, 0, 100);
+
+		// 항목별로 추출하여 출력
+		int year = c.get(Calendar.YEAR);
+		System.out.println(year);
+
+		int month = c.get(Calendar.MONTH) + 1; // (주의) 월은 0부터 11까지 관리됨
+		System.out.println(month);
+
+		// int day = c.get(Calendar.DATE);
+		int day = c.get(Calendar.DAY_OF_MONTH);
+		System.out.println(day);
+
+		// int hour = c.get(Calendar.HOUR);
+		int hour = c.get(Calendar.HOUR_OF_DAY);
+		System.out.println(hour);
+
+		int minute = c.get(Calendar.MINUTE);
+		System.out.println(minute);
+
+		int second = c.get(Calendar.SECOND);
+		System.out.println(second);
 	}
 }
