12e96b9da79cf6e877a378b389d268adbd4e5625
jhcho <jhcho@LenovoIDEAPAD>
2023-07-12 11:07:29 +0900

    day18

--- day18/src/api/util/random/Test03.java
+++ day18/src/api/util/random/Test03.java
@@ -1,5 +1,6 @@
 package api.util.random;
 
+import java.text.DecimalFormat;
 import java.util.Random;
 
 public class Test03 {
@@ -9,9 +10,15 @@ public class Test03 {
 		// = 검사하는 서버와 입력하는 사용자가 동일한 랜덤값을 가져야 한다
 		
 		// 사용자(클라이언트)
+		String serial = "0000-0000-0000-0000";
 		Random r = new Random();
-		r.setSeed(1L);
+		
+		long seed = System.currentTimeMillis() / 60000;
+		seed += serial.hashCode();
+		r.setSeed(seed);
+		
 		int otp = r.nextInt(1000000) + 0;
+		DecimalFormat fmt = new DecimalFormat("000000");
 		System.out.println("클라이언트 = " + otp);
 	}
 }
