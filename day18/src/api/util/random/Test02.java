3eeed1e03e48e4552579dc5076c5b1afb77d6f58
jhcho <jhcho@LenovoIDEAPAD>
2023-07-12 10:46:57 +0900

    day18

---
+++ day18/src/api/util/random/Test02.java
@@ -0,0 +1,14 @@
+package api.util.random;
+
+import java.util.Random;
+
+public class Test02 {
+	public static void main(String[] args) {
+		// 시드를 제어하여 랜덤을 조작하는 프로그램
+		// = OTP(One Time Password)
+		// = 검사하는 서버와 입력하는 사용자가 동일한 랜덤값을 가져야 한다
+		
+		// 서버
+		Random r = new Random();
+	}
+}
---
+++ day18/src/api/util/random/Test03.java
@@ -0,0 +1,12 @@
+package api.util.random;
+
+public class Test03 {
+	public static void main(String[] args) {
+		// 시드를 제어하여 랜덤을 조작하는 프로그램
+		// = OTP(One Time Password)
+		// = 검사하는 서버와 입력하는 사용자가 동일한 랜덤값을 가져야 한다
+		
+		// 사용자(클라이언트)
+		Random r = new Random();
+	}
+}
