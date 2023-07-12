5acc6e0537911913a5add1aaf4324e1b3b89df46
jhcho <jhcho@LenovoIDEAPAD>
2023-07-12 11:10:34 +0900

    day18

---
+++ day18/src/api/util/random/Test04.java
@@ -0,0 +1,12 @@
+package api.util.random;
+
+import java.util.Random;
+
+public class Test04 {
+	public static void main(String[] args) {
+		// 여러가지 Random의 명령들
+		Random r = new Random();
+		
+		System.out.println(r.nextBoolean());
+	}
+}
