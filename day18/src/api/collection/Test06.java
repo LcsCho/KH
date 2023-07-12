e41bcd0eafbb0a709e9ab4dd86cd8a562ed63c44
jhcho <jhcho@LenovoIDEAPAD>
2023-07-12 15:44:23 +0900

    day18

--- day18/src/api/collection/Test06.java
+++ day18/src/api/collection/Test06.java
@@ -7,22 +7,22 @@ public class Test06 {
 	public static void main(String[] args) {
 		ArrayList<String> wordList = new ArrayList<>();
 		System.out.println("<끝말잇기 게임>");
-		String word = "자바";
-		wordList.add(word);
+		String currentWord = "자바";
+		wordList.add(currentWord);
 
-		final String regex = "^[가-힣]{2,6}$";
+		final String koreanWordRegex = "^[가-힣]{2,6}$";
 		Scanner sc = new Scanner(System.in);
 		while (true) {
-			System.out.println("제시어: " + word);
+			System.out.println("제시어: " + currentWord);
 			System.out.print("단어를 입력: ");
-			String input = sc.next();
+			String inputWord = sc.next();
 
-			boolean isCorrect = word.charAt(word.length() - 1) == input.charAt(0);
-			boolean isMatch = input.matches(regex);
+			boolean isCorrect = currentWord.charAt(currentWord.length() - 1) == inputWord.charAt(0);
+			boolean isMatch = inputWord.matches(koreanWordRegex);
 
 			if (isCorrect && isMatch) {
-				wordList.add(input);
-				word = input;
+				wordList.add(inputWord);
+				currentWord = inputWord;
 			} else {
 				sc.close();
 				break;
