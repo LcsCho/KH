00fe36c1e798d740f0d2ccb32f6ca6d7d956c8d0
jhcho <jhcho@LenovoIDEAPAD>
2023-07-12 18:04:29 +0900

    day18

---
+++ day18/src/api/collection/Test10.java
@@ -0,0 +1,16 @@
+package api.collection;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+public class Test10 {
+	public static void main(String[] args) {
+		// ArrayList 말고 다른 List의 종류
+		// - ArrayList는 데이터가 배열 형태로 존재하는 리스트
+		// - LinkedList는 데이터가 연결 형태로 존재하는 리스트
+		
+		List<String> a = new ArrayList<>();
+		List<String> b = new LinkedList<>();
+	}
+}
