3de38be71d19a3da9cfd570028f9731db734802a
jhcho <jhcho@LenovoIDEAPAD>
2023-07-12 15:42:38 +0900

    day18

--- day18/src/api/collection/Test06.java
+++ day18/src/api/collection/Test06.java
@@ -5,10 +5,10 @@ import java.util.Scanner;
 
 public class Test06 {
 	public static void main(String[] args) {
-		ArrayList<String> list = new ArrayList<>();
+		ArrayList<String> wordList = new ArrayList<>();
 		System.out.println("<끝말잇기 게임>");
 		String word = "자바";
-		list.add(word);
+		wordList.add(word);
 
 		final String regex = "^[가-힣]{2,6}$";
 		Scanner sc = new Scanner(System.in);
@@ -21,7 +21,7 @@ public class Test06 {
 			boolean isMatch = input.matches(regex);
 
 			if (isCorrect && isMatch) {
-				list.add(input);
+				wordList.add(input);
 				word = input;
 			} else {
 				sc.close();
@@ -30,6 +30,6 @@ public class Test06 {
 		}
 		System.out.println("게임 오버!");
 		System.out.print("단어 목록: ");
-		System.out.println(list);
+		System.out.println(wordList);
 	}
 }
