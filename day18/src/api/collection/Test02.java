89d09253293ec886b1a92c7f2edc0e8e56bd1cea
jhcho <jhcho@LenovoIDEAPAD>
2023-07-12 13:04:58 +0900

    day18

---
+++ day18/src/api/collection/Test02.java
@@ -0,0 +1,19 @@
+package api.collection;
+
+import java.util.ArrayList;
+
+public class Test02 {
+	public static void main(String[] args) {
+		// List 저장소
+		// = 순서가 있는 저장소
+		// = 선형구조(시작지점과 끝지점이 있는 형태)
+		// = ArrayList, LinkedList, Vector
+		
+		// 자료형(Generic Type)을 지정하지 않으면 경고
+		// ArrayList<> list = new ArrayList<>(); 
+		
+		// 자료형을 지정하면서 생성
+		ArrayList<String> list = new ArrayList<String>();
+		
+	}
+}
