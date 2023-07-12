8e6d58f7be5b7b074b9bb994566aa5266a439a59
jhcho <jhcho@LenovoIDEAPAD>
2023-07-12 11:11:53 +0900

    day18

--- day18/src/api/util/random/Test04.java
+++ day18/src/api/util/random/Test04.java
@@ -8,5 +8,9 @@ public class Test04 {
 		Random r = new Random();
 		
 		System.out.println(r.nextBoolean());
+		System.out.println(r.nextInt());
+		System.out.println(r.nextLong());
+		System.out.println(r.nextFloat()); // 0이상 1미만
+		System.out.println(r.nextDouble()); // 0이상 1미만
 	}
 }
