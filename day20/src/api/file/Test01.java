a235aeaf0680b8af042c6e907957219e7d1406a6
jhcho <jhcho@LenovoIDEAPAD>
2023-07-14 10:59:11 +0900

    day20

--- day20/src/api/file/Test01.java
+++ day20/src/api/file/Test01.java
@@ -2,6 +2,13 @@ package api.file;
 
 public class Test01 {
 	public static void main(String[] args) {
-		
+		// 파일(File) 제어
+		// - 파일은 무엇인가?			글자를 저장하는 저장소
+		//						파일이 크다는 것은 보관하는 글자가 많다는 소리
+		//						글자 하나 당 1byte (자바에서 byte로 사용)
+		// - 폴더는 무엇인가?			파일을 구분하기 위한 장치
+		// 						디렉터리(directory)라고 부름
+		//						크기가 없음 (내부 파일의 합계)
+		// - 자바에서 파일과 폴더는 어떻게 처리하는가?
 	}
 }
