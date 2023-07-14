65cb89f579525a22f49fde928179329107ad56ed
jhcho <jhcho@LenovoIDEAPAD>
2023-07-14 10:42:07 +0900

    day20

---
+++ day20/src/api/collection4/Test02.java
@@ -0,0 +1,14 @@
+package api.collection4;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class Test02 {
+	public static void main(String[] args) {
+		// 큐(Queue)
+		// = 대기열을 구현할 때 사용하는 저장소
+		
+		// List<String> list = new LinkedList<>();
+		Queue<String> queue = new LinkedList<>();
+	}
+}
