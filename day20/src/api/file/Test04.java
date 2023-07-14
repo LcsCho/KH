23573c17e90bfc8dc5c4bec7142ddbd26fb48114
LcsCho <chojunhee98@gmail.com>
2023-07-14 13:58:32 +0900

    day20

---
+++ day20/src/api/file/Test04.java
@@ -0,0 +1,9 @@
+package api.file;
+
+public class Test04 {
+	public static void main(String[] args) {
+		// 경로(path)
+		// = PC마다 설치된 디스크 환경이 다를 수 있다
+		// = D:/와 같은 절대적인 위치는 다른 PC에서 인식하지 못할 수 있다.
+	}
+}
