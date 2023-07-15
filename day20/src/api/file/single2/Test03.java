73d9b587b0a365fc7973e81a06713addd645810e
LcsCho <chojunhee98@gmail.com>
2023-07-15 14:05:43 +0900

    day20

---
+++ day20/src/api/file/single2/Test03.java
@@ -0,0 +1,34 @@
+package api.file.single2;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class Test03 {
+	public static void main(String[] args) throws IOException {
+
+		byte[] buffer = new byte[4096];
+
+		File readTarget = new File("C:/Temp/origin.txt");
+		FileInputStream readStream = new FileInputStream(readTarget);
+
+		File writeTarget = new File("./sample/test3.txt");
+		FileOutputStream writeStream = new FileOutputStream(writeTarget);
+
+		long start = System.currentTimeMillis();
+
+		while (true) {
+			int size = readStream.read(buffer);
+			if (size == -1)
+				break;
+			writeStream.write(buffer);
+		}
+
+		long end = System.currentTimeMillis();
+
+		System.out.println("실행시간 " + (end - start) + "초");
+		readStream.close();
+		writeStream.close();
+	}
+}
