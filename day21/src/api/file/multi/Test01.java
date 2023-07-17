795a0df33ceace1a3aec7e007bc7f0e6e0c9bac7
LcsCho <chojunhee98@gmail.com>
2023-07-17 12:35:18 +0900

    day21

---
+++ day21/src/api/file/multi/Test01.java
@@ -0,0 +1,8 @@
+package api.file.multi;
+
+public class Test01 {
+	public static void main(String[] args) {
+		// 멀티 바이트 입출력
+		// = 원시형 데이터 및 크기가 정해진 예외적인 데이터들
+	}
+}
--- day21/src/api/file/single2/Test04.java
+++ day21/src/api/file/single2/Test04.java
@@ -4,15 +4,16 @@ import java.io.File;
 
 public class Test04 {
 	public static void main(String[] args) {
-		// 파일 삭제는 가능하지만 디렉터리는 비어있는 것만 삭제가 가능
 		
+//		파일 삭제는 가능하지만 디렉터리는 비어있는것만 삭제가 가능
 		
-		// File target = new File("C:/Temp/origin.txt");
-		// target.delete();
+//		File target = new File("D:/kh로고.png");
+//		target.delete();
 		
-		File target = new File("C:/Temp/origin.txt");
-		// target.delete()
+		File target = new File("D:/dummy");
+//		target.delete();
 		
 		FileUtil.delete(target);
+		
 	}
 }
