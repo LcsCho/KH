2a1c3fba0f28c4990913f6d0de2ed3b544ed4df5
LcsCho <chojunhee98@gmail.com>
2023-07-17 13:15:18 +0900

    day21

--- day21/src/api/file/multi/Test03.java
+++ day21/src/api/file/multi/Test03.java
@@ -5,7 +5,6 @@ import java.io.DataInputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
-import java.text.DecimalFormat;
 
 public class Test03 {
 	public static void main(String[] args) throws IOException {
