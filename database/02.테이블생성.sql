20b1ce72e5bbd64654284c24dea034d83699bf54
Lucas Cho <chojunhee98@gmail.com>
2023-07-18 16:28:53 +0900

    Update 02.테이블생성.sql

--- database/02.테이블생성.sql
+++ database/02.테이블생성.sql
@@ -1,22 +1,25 @@
 -- 문제1: 포켓몬 정보
-CREATE TABLE pocketmon(
+DROP TABLE pokemon;
+CREATE TABLE pokemon(
 	no number,
 	name varchar2(21),
 	type varchar2(9)
 );
 -- 문제2: 월드컵 정보
+DROP TABLE worldcup;
 CREATE TABLE worldcup(
-	grade number,
-	name varchar2(34),
-	nation varchar2(54),
-	goal number
+   grade number,
+   name varchar2(34),
+   nation varchar2(54),
+   goal number
 );
 -- 문제3: 메달리스트 정보
-CREATE TABLE medallist(
-	name varchar2(66),
-	game varchar2(50),
-	season varchar2(6),
-	gold number,
-	silver number,
-	bronze number
+DROP TABLE medalist;
+CREATE TABLE medalist(
+   name varchar2(66),
+   game varchar2(50),
+   season varchar2(6),
+   gold number,
+   silver number,
+   bronze number
 );
