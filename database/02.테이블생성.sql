01fdac26fb4b9e8a16c8627e55218a9e0ba18e0c
Lucas Cho <chojunhee98@gmail.com>
2023-07-18 16:22:30 +0900

    Create 02.테이블생성.sql

---
+++ database/02.테이블생성.sql
@@ -0,0 +1,22 @@
+-- 문제1: 포켓몬 정보
+CREATE TABLE pocketmon(
+	no number,
+	name varchar2(21),
+	type varchar2(9)
+);
+-- 문제2: 월드컵 정보
+CREATE TABLE worldcup(
+	grade number(,
+	name varchar2(34),
+	nation varchar2(54),
+	goal number
+);
+-- 문제3: 메달리스트 정보
+CREATE TABLE medallist(
+	name varchar2(66),
+	game varchar2(50),
+	season varchar2(6),
+	gold number,
+	silver number,
+	bronze number
+);
