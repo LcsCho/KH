cf7fd09b121e3449d87bfffc8e4d960416da22b0
LcsCho <chojunhee98@gmail.com>
2023-07-21 17:36:26 +0900

    jdbc01

--- jdbc01/src/jdbc/Test08.java
+++ jdbc01/src/jdbc/Test08.java
@@ -7,6 +7,10 @@ import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
 public class Test08 {
 	public static void main(String[] args) {
+		// 회원 가입 프로그램
+		
+		// 데이터
+		// tip, 오라클에 추가하는 문자열은 null이든 ""이든 모두 null로 추가됨
 		Scanner sc = new Scanner(System.in);
         System.out.print("아이디: ");
         String memberId = sc.nextLine();
