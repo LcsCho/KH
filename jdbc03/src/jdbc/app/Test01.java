47f1eaac9351ccaa44cc2fa8234ad7302255183e
LcsCho <chojunhee98@gmail.com>
2023-07-24 15:46:35 +0900

    jdbc03

--- jdbc03/src/jdbc/app/Test01.java
+++ jdbc03/src/jdbc/app/Test01.java
@@ -2,6 +2,13 @@ package jdbc.app;
 
 public class Test01 {
 	public static void main(String[] args) {
+		// 포켓몬스터 등록
+		
+		// 데이터 
+		int no = 55;
+		String name = "어쩌구";
+		String type = "저쩌구";
+		
 		
 	}
 }
