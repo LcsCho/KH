8ae7b14fb1655860f5bcbfb6eab36c6629492bb9
LcsCho <chojunhee98@gmail.com>
2023-07-26 16:00:22 +0900

    spring03

--- spring03controller/src/main/java/com/kh/spring03/QuizController.java
+++ spring03controller/src/main/java/com/kh/spring03/QuizController.java
@@ -19,4 +19,31 @@ public class QuizController {
 	public String lotto() {
 		return "주사위: " + (r.nextInt(45) + 1);
 	}
+	
+	@RequestMapping("/square")
+	public String square(int a) {
+		return a + "의 제곱은" + (a * a);
+	}
+	
+	@RequestMapping("/subway")
+	public String subway(int year) {
+		if (year <= 1958 || year >= 2018) return "요금은 무료입니다.";
+		else if (year < 2010) return "요금은 1250원 입니다.";
+		else if (year < 2015) return "요금은 800원 입니다.";
+		else if (year < 2018) return "요금은 500원입니다.";
+		else return "요금은 무료입니다.";
+	}
+	
+	@RequestMapping("/leap")
+	public String leap(int year) {
+        boolean isLeapYear = (year % 4 == 0 && (year % 100 != 0 || year % 400 == 0));
+        
+        if (isLeapYear) return year + "년은 윤년입니다.";
+        else return year + "년은 평년입니다.";
+	}
+	
+	@RequestMapping("/circle")
+	public String circle(float r) {
+		return "원의 넓이는 " + (r * r * 3.14);
+	}
 }
