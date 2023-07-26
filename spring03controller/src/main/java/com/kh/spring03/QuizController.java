f4051952af84d884b69d069cd841eb9e6da180dd
LcsCho <chojunhee98@gmail.com>
2023-07-26 14:42:47 +0900

    spring03

--- spring03controller/src/main/java/com/kh/spring03/QuizController.java
+++ spring03controller/src/main/java/com/kh/spring03/QuizController.java
@@ -7,14 +7,14 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class QuizController {
-	
+
 	Random r = new Random();
-	
+
 	@RequestMapping("/dice")
 	public int dice() {
 		return r.nextInt(6) + 1;
 	}
-	
+
 	@RequestMapping("/lotto")
 	public int lotto() {
 		return r.nextInt(45) + 1;
