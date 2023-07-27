5ffb2d16193fb9ec5d429380be1b87a75a960e9f
LcsCho <chojunhee98@gmail.com>
2023-07-27 20:56:52 +0900

    spring05

--- spring05jdbc/src/main/java/com/kh/spring05/controller/PokemonController.java
+++ spring05jdbc/src/main/java/com/kh/spring05/controller/PokemonController.java
@@ -1,8 +1,11 @@
 package com.kh.spring05.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.kh.spring05.dao.PokemonDao;
@@ -26,5 +29,32 @@ public class PokemonController {
 
 		if (result) return "포켓몬스터 정보 변경 완료";
 		else return "해당 번호는 존재하지 않습니다.";
+	}
+	@RequestMapping("/delete")
+	public String delete(@RequestParam int no) {
+		boolean result = dao.delete(no);
+		
+		if (result) return "삭제 완료";
+		else return "존재하지 않는 몬스터";
+	}
+	@RequestMapping("/list")
+	public String list() {
+		List<PokemonDto> list = dao.selectList();
+		StringBuffer buffer = new StringBuffer();
+		for (PokemonDto dto : list) {
+			buffer.append(dto);
+			buffer.append("<br>");
+		}
+		return buffer.toString();
+	}
+	@RequestMapping("/selectOne")
+	public String selectOne(@RequestParam int no) {
+
+		if (dao == null) return "없는 번호의 몬스터";
+		else {
+			StringBuffer buffer = new StringBuffer();
+			buffer.append(dao.selectOne(no));
+			return buffer.toString();
+		}
 	}
 }
--- spring05jdbc/src/main/java/com/kh/spring05/dao/PokemonDao.java
+++ spring05jdbc/src/main/java/com/kh/spring05/dao/PokemonDao.java
@@ -1,10 +1,13 @@
 package com.kh.spring05.dao;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
 import com.kh.spring05.dto.PokemonDto;
+import com.kh.spring05.mapper.PokemonMapper;
 
 // DAO 클래스
 // = Spring에 등록해야됨
@@ -14,6 +17,9 @@ import com.kh.spring05.dto.PokemonDto;
 public class PokemonDao {
 	@Autowired // (주의) 등록을 하지 않으면 절대로 주지 않음
 	private JdbcTemplate jdbcTemplate;
+	
+	@Autowired
+	private PokemonMapper mapper;
 	
 	public void insert(PokemonDto dto) {
 		String sql = "insert into pokemon(no, name, type)"
@@ -29,5 +35,24 @@ public class PokemonDao {
 				+ " where no = ?";
 		Object[] data = {dto.getName(), dto.getType(), dto.getNo()};
 		return jdbcTemplate.update(sql, data) > 0;
+	}
+
+	public boolean delete(int no) {
+		String sql = "delete pokemon where no = ?";
+		Object[] data = {no};
+		return jdbcTemplate.update(sql, data) > 0;
+	}
+	
+	public List<PokemonDto> selectList() {
+		String sql = "select * from pokemon order by no asc";
+		return jdbcTemplate.query(sql, mapper);
+		
+	}
+	
+	public PokemonDto selectOne(int no) {
+		String sql = "select * from pokemon where no = ?";
+		Object[] data = {no};
+		List<PokemonDto> list = jdbcTemplate.query(sql, mapper, data);
+		return list.isEmpty() ? null : list.get(0);
 	}
 }
