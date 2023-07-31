5ffdc8944bce2e91b9974b103647098188a0447b
LcsCho <chojunhee98@gmail.com>
2023-07-31 14:43:05 +0900

    spring09

---
+++ spring09view/src/main/java/com/kh/spring09/controller/ViewController.java
@@ -0,0 +1,30 @@
+package com.kh.spring09.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+// 화면을 보여주기 위한 컨트롤러
+// @RestController - 데이터를 보내주는 컨트롤러
+@Controller // 화면을 보내주는 컨트롤러
+public class ViewController {
+	
+	@RequestMapping("/hello")
+	public String hello() {
+		return "/WEB-INF/views/hello.jsp";
+	}
+	
+	@RequestMapping("/heading")
+	public String heading() {
+		return "/WEB-INF/views/heading.jsp";
+	}
+	
+	@RequestMapping("/link")
+	public String link() {
+		return "/WEB-INF/views/link.jsp";
+	}
+	
+	@RequestMapping("/image")
+	public String image() {
+		return "/WEB-INF/views/image.jsp";
+	}
+}
