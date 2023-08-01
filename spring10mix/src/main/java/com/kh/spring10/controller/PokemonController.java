d6066e3f453a710cb77ddf16bdf634c78b5e5074
LcsCho <chojunhee98@gmail.com>
2023-08-01 21:30:25 +0900

    spring10

--- spring10mix/src/main/java/com/kh/spring10/controller/PokemonController.java
+++ spring10mix/src/main/java/com/kh/spring10/controller/PokemonController.java
@@ -1,8 +1,28 @@
 package com.kh.spring10.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.kh.spring10.dao.PokemonDao;
+import com.kh.spring10.dto.PokemonDto;
 
 @Controller
+@RequestMapping("/pokemon")
 public class PokemonController {
+	@Autowired private PokemonDao dao;
 	
+	@RequestMapping("/detail")
+	public String detail(Model model, @RequestParam int no) {
+		PokemonDto dto = dao.selectOne(no);
+		if (dto == null) return "없는 번호의 몬스터";
+		else {
+			model.addAttribute("no", dto.getNo());
+			model.addAttribute("name", dto.getName());
+			model.addAttribute("type", dto.getType());
+			return "/WEB-INF/views/test03.jsp";
+		}
+	}
 }
---
+++ spring10mix/src/main/webapp/WEB-INF/views/test03.jsp
@@ -0,0 +1,15 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>포켓몬 정보</title>
+</head>
+<body>
+	<h1>세부 정보</h1>
+	<h2>번호: ${no}</h2>
+	<h2>이름: ${name}</h2>
+	<h2>타입: ${type}</h2>
+</body>
+</html>
