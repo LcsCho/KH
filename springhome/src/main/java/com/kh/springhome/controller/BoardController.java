52d9f5574d05c2876d10a3d89128372048f985f9
LcsCho <chojunhee98@gmail.com>
2023-09-07 13:38:00 +0900

    springhome

--- springhome/src/main/java/com/kh/springhome/controller/BoardController.java
+++ springhome/src/main/java/com/kh/springhome/controller/BoardController.java
@@ -235,6 +235,16 @@ public class BoardController {
 		return "/WEB-INF/views/board/listMention.jsp";
 	}
 	
+	// 관리자가 이용하는 선택삭제 기능
+	@PostMapping("/deleteByAdmin") 
+	public String deleteByAdmin(
+			@RequestParam List<Integer> boardNoList) {
+		for (int boardNo : boardNoList) {
+			boardDao.delete(boardNo);
+		}
+		return "redirect:list";
+	}
+	
 }
 
 
