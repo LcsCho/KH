059da108ef9c003570f416095e431392ebed08f0
LcsCho <chojunhee98@gmail.com>
2023-09-11 18:43:42 +0900

    springhome

--- springhome/src/main/java/com/kh/springhome/rest/ReplyRestController.java
+++ springhome/src/main/java/com/kh/springhome/rest/ReplyRestController.java
@@ -15,36 +15,36 @@ import com.kh.springhome.dao.BoardDao;
 import com.kh.springhome.dao.ReplyDao;
 import com.kh.springhome.dto.ReplyDto;
 
-// @CrossOrigin
+//@CrossOrigin
 @RestController
 @RequestMapping("/rest/reply")
 public class ReplyRestController {
-	
+
 	@Autowired
 	private ReplyDao replyDao;
 	
 	@Autowired
 	private BoardDao boardDao;
 	
-	// 댓글 등록
-	// - 사용자가 입력한 내용을 특정 번호의 게시글 아래로 등록하도록 구현
-	// - 사용자는 댓글내용(replyContent)만 작성
-	// - 댓글번호(replyNo)는 시퀀스로 생성
-	// - 작성자(replyWriter)는 세션에서 조회
-	// - 작성일(replyTime)은 DB에서 sysdate로 등록
-	// - 원본글번호(replyOrigin)은 사용자 몰래 전송시켜야 한다
+	//댓글 등록
+	//- 사용자가 입력한 내용을 특정 번호의 게시글 아래로 등록하도록 구현
+	//- 사용자는 댓글내용(replyContent)만 작성
+	//- 댓글번호(replyNo)는 시퀀스로 생성
+	//- 작성자(replyWriter)는 세션에서 조회
+	//- 작성일(replyTime)은 DB에서 sysdate로 등록
+	//- 원본글번호(replyOrigin)은 사용자 몰래 전송시켜야 한다
 	@PostMapping("/insert")
 	public void insert(@ModelAttribute ReplyDto replyDto, HttpSession session) {
 		int replyNo = replyDao.sequence();
 		replyDto.setReplyNo(replyNo);
 		
-		String memberId = (String) session.getAttribute("name");
+		String memberId = (String)session.getAttribute("name");
 		replyDto.setReplyWriter(memberId);
 		
 		replyDao.insert(replyDto);
 		
-		// 댓글 개수 업데이트
-		boardDao.updateBoardReadcount(replyDto.getReplyOrigin());
+		//댓글 개수 업데이트
+		boardDao.updateBoardReplycount(replyDto.getReplyOrigin());
 	}
 	
 	@PostMapping("/list")
@@ -58,8 +58,8 @@ public class ReplyRestController {
 		ReplyDto replyDto = replyDao.selectOne(replyNo);
 		replyDao.delete(replyNo);
 		
-		// 댓글 개수 업데이트
-		boardDao.updateBoardReadcount(replyDto.getReplyOrigin());
+		//댓글 개수 업데이트
+		boardDao.updateBoardReplycount(replyDto.getReplyOrigin());
 	}
 	
 	@PostMapping("/edit")
@@ -67,6 +67,10 @@ public class ReplyRestController {
 		replyDao.update(replyDto);
 	}
 	
-	
-	
 }
+
+
+
+
+
+
