0a61279e4d1109f8d204c4114478f4b06d06b65d
LcsCho <chojunhee98@gmail.com>
2023-09-15 11:14:45 +0900

    spring

--- spring12file/src/main/java/com/kh/spring12/controller/FileRestController.java
+++ spring12file/src/main/java/com/kh/spring12/controller/FileRestController.java
@@ -5,6 +5,8 @@ import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.util.Map;
 
+import javax.annotation.PostConstruct;
+
 import org.apache.commons.io.FileUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.ByteArrayResource;
@@ -18,6 +20,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
+import com.kh.spring12.configuration.FileUploadProperties;
 import com.kh.spring12.dao.AttachDao;
 import com.kh.spring12.dto.AttachDto;
 
@@ -28,6 +31,18 @@ import com.kh.spring12.dto.AttachDto;
 
 public class FileRestController {
    @Autowired AttachDao attachDao;
+   
+   // 미리 작성해둔 커스텀 속성을 불러와서 디렉터리 객체까지 생성
+   @Autowired
+   private FileUploadProperties props;
+   
+   private File dir;
+   
+   @PostConstruct // 모든 로딩이 끝나면 자동으로 실행되는 메서드
+   public void init() {
+	   dir = new File(props.getHome());
+	   dir.mkdirs();
+   }
    
    @PostMapping("/upload")
    //json
@@ -44,10 +59,6 @@ public class FileRestController {
       
       //[2] 시퀀스 번호를 파일명으로 하여 실제 파일을 저장한다
       //-같은이름 충돌 방지
-      String home = System.getProperty("user.home");
-      File dir = new File(home,"upload");//저장할 디렉토리
-      dir.mkdirs();//디렉토리생성
-      
       File target = new File(dir,String.valueOf(attachNo));//저장할 파일 객체
       attach.transferTo(target);//저장
       
@@ -73,8 +84,6 @@ public class FileRestController {
          //throw new NoTargetException("파일 없음");//내가만든 예외로 통합
          return ResponseEntity.notFound().build();//404 반환
       }
-      String home = System.getProperty("user.home");
-      File dir = new File(home, "upload");
       File target = new File(dir, String.valueOf(attachDto.getAttachNo()));
       
       byte[] data = FileUtils.readFileToByteArray(target);//실제파일정보 불러오기
@@ -90,7 +99,5 @@ public class FileRestController {
                .build().toString()
             )
          .body(resource);
-      
    }
-
 }
