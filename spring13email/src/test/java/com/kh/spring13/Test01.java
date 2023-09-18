c399803d1009cbc8db157799ba3a1d5efdbb3ef3
LcsCho <chojunhee98@gmail.com>
2023-09-18 11:02:37 +0900

    spring13

--- spring13email/src/test/java/com/kh/spring13/Test01.java
+++ spring13email/src/test/java/com/kh/spring13/Test01.java
@@ -1,5 +1,16 @@
 package com.kh.spring13;
 
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@SpringBootTest
 public class Test01 {
 	
+	@Test
+	public void test() {
+		
+	}
 }
