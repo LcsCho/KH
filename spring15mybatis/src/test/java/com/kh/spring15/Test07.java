9fce00da6ed6a8eed2fde891a09a1793ac99f91c
LcsCho <chojunhee98@gmail.com>
2023-10-13 15:01:21 +0900

    spring15

--- spring15mybatis/src/test/java/com/kh/spring15/Test07.java
+++ spring15mybatis/src/test/java/com/kh/spring15/Test07.java
@@ -27,8 +27,19 @@ public class Test07 {
 //		param.put("bookAuthor", "조");
 //		param.put("bookTitle", "결핵");
 //		param.put("bookPublisher", "보라매");
-		param.put("bookPriceMin", 10);
-		param.put("bookPriceMax", 100000);
+//		param.put("bookPriceMin", 10);
+//		param.put("bookPriceMax", 100000);
+//		param.put("bookPublicationBegin", "2023-01-01");
+//		param.put("bookPublicationEnd", "2023-12-31");
+//		param.put("bookPageMin", 500);
+//		param.put("bookPageMaxya", 1000);
+//		param.put("bookGenre", "판타지"); // 장르가 한 개였다면...(X)
+//		param.put("bookGenreList", List.of("판타지", "공포", "의학", "질병")); // 장르가 여러 개라면...(O)
+		
+//		param.put("orderList", List.of("book_id asc")); // 번호순
+		
+//		param.put("begin", 1);
+//		param.put("end", 10);
 		
 		List<BookDto> list = sqlSession.selectList("book.complexSearch", param);
 		log.debug("검색 결과 = {}개", list.size());
