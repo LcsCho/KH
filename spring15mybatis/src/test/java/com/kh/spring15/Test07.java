333754d27ab0088963c5d672562f5efeb363b3f6
LcsCho <chojunhee98@gmail.com>
2023-10-13 11:02:57 +0900

    spring15

---
+++ spring15mybatis/src/test/java/com/kh/spring15/Test07.java
@@ -0,0 +1,37 @@
+package com.kh.spring15;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.ibatis.session.SqlSession;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import com.kh.spring15.dto.BookDto;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@SpringBootTest
+public class Test07 {
+	
+	@Autowired
+	private SqlSession sqlSession;
+	
+	@Test
+	public void test() {
+		Map<String, Object> param = new HashMap<>();
+//		param.put("bookId", 3);
+//		param.put("bookAuthor", "조");
+//		param.put("bookTitle", "결핵");
+//		param.put("bookPublisher", "보라매");
+		param.put("bookPriceMin", 10);
+		param.put("bookPriceMax", 100000);
+		
+		List<BookDto> list = sqlSession.selectList("book.complexSearch", param);
+		log.debug("검색 결과 = {}개", list.size());
+		for (BookDto dto : list) log.debug("dto = {}", dto);
+	}
+}
