3e08c702d96d7c9210ad44abf02557fc1065059f
LcsCho <chojunhee98@gmail.com>
2023-10-16 15:23:47 +0900

    spring18

---
+++ spring18security/src/test/java/com/kh/spring18/Test04.java
@@ -0,0 +1,17 @@
+package com.kh.spring18;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@SpringBootTest
+public class Test04 {
+
+	@Test
+	public void test() {
+		// spring-security에서 제공하는 라이브러리를 사용하여 암호화를 진행(brypt)
+		
+	}
+}
