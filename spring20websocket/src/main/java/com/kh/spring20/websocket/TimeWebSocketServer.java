e2e5b94bb251c4bac54f0114a3012e10784fd975
LcsCho <chojunhee98@gmail.com>
2023-10-20 11:15:52 +0900

    spring20

---
+++ spring20websocket/src/main/java/com/kh/spring20/websocket/TimeWebSocketServer.java
@@ -0,0 +1,31 @@
+package com.kh.spring20.websocket;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+import org.springframework.stereotype.Service;
+import org.springframework.web.socket.CloseStatus;
+import org.springframework.web.socket.WebSocketSession;
+import org.springframework.web.socket.handler.TextWebSocketHandler;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Service
+public class TimeWebSocketServer extends TextWebSocketHandler{
+//public class TimeWebSocketServer implements WebSocketHandler{
+
+	@Override
+	public void afterConnectionEstablished(WebSocketSession session) throws Exception {
+		LocalDate today = LocalDate.now();
+		LocalTime now = LocalTime.now();
+		log.debug(today.toString() + " " + now.toString().substring(0, 8) + " 사용자 접속 성공");
+	}
+
+	@Override
+	public void afterConnectionClosed(WebSocketSession session, CloseStatus status) throws Exception {
+		LocalDate today = LocalDate.now();
+		LocalTime now = LocalTime.now();
+		log.debug(today.toString() + " " + now.toString().substring(0, 8) + " 사용자 접속 종료");
+	}
+}
